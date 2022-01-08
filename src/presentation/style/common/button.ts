d8164528a13884efb179481f8378eb6204ed64e9
jynam <njy1007@gmail.com>
2022-01-08 20:51:00 +0900

    🔨 refacter: BTN -> BUTTON

--- src/presentation/style/common/button.ts
+++ src/presentation/style/common/button.ts
@@ -1,12 +1,12 @@
 import { css } from 'styled-components';
 import { COLOR } from '@style/common/color';
 
-export const CORAL_MAIN_BTN = css`
+export const CORAL_MAIN_BUTTON = css`
   background-color: ${COLOR.CORAL_MAIN};
   color: ${COLOR.WHITE};
 `;
 
-export const BIG_BTN = css`
+export const BIG_BUTTON = css`
   width: 354px;
   height: 58px;
   border-radius: 14px;

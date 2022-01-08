86b4a09a3384a2c930760bdcc922004310450fdf
jynam <njy1007@gmail.com>
2022-01-08 20:57:12 +0900

    🔨 refacter: 네이밍 변경

--- src/presentation/style/common/button.ts
+++ src/presentation/style/common/button.ts
@@ -6,7 +6,7 @@ export const CORAL_MAIN_BUTTON = css`
   color: ${COLOR.WHITE};
 `;
 
-export const BIG_BUTTON = css`
+export const FULL_WIDTH_BUTTON = css`
   width: 100%;
   height: 58px;
   border-radius: 14px;

43c752a31bb4af210be86cd4e1a9bacce7ad3f04
seojin kim <001106ksj@gmail.com>
2022-01-09 03:13:20 +0900

    ✨feat: TeamIssue 창에서 dataprovider을 사용하여 api를 호출해보기

--- src/infrastructure/provider/team.ts
+++ src/infrastructure/provider/team.ts
@@ -1,27 +1,5 @@
+import { IssueData } from './types/team';
+
 export interface TeamService {
   getIssueInfo(teamID: string, issueID: string): Promise<IssueData>;
 }
-
-type IssueData = {
-  createdAt: string;
-  title: string;
-  category: string;
-  team: TeamData;
-  issueList: IssueDetail[];
-  writer: string;
-  thumbnail?: string;
-};
-
-type TeamData = {
-  teammates: string[];
-  thumbnail: string;
-  title: string;
-};
-
-type IssueDetail = {
-  writer: string;
-  target: string;
-  body: string;
-  createdAt: string;
-  keywordList: string[];
-};
--- src/presentation/pages/Team/Issue/index.tsx
+++ src/presentation/pages/Team/Issue/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { api } from '@provider/index';
+import { IssueData } from '@provider/types/team';
 
 function TeamIssue() {
-  const { issueID } = useParams();
-  return <div>팀원소개서 상세, 이슈 번호는 {issueID}</div>;
+  const { teamID, issueID } = useParams();
+  const [issue, setIssue] = useState<IssueData | null>(null);
+  const [isValidating, setIsValidating] = useState(false);
+
+  useEffect(() => {
+    if (!teamID || !issueID) return;
+    (async () => {
+      setIsValidating(true);
+      const data = await api.teamService.getIssueInfo(teamID, issueID);
+      setIssue(data);
+      setIsValidating(false);
+    })();
+  }, [teamID, issueID]);
+
+  return (
+    <>
+      <div>팀원소개서 상세, 이슈 번호는 {issueID}</div>
+      {isValidating && <div>로딩중</div>}
+      <div>{issue && issue.title}</div>
+    </>
+  );
 }
 
 export default TeamIssue;

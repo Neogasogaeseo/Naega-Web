76e2ad8b81c595a73c3b427c11a3c8b5440e65ae
100Gyeon <bjy9966@naver.com>
2022-01-09 01:37:33 +0900

    📝chore: tempData로 임시 렌더링

--- src/presentation/components/TeamList/index.tsx
+++ src/presentation/components/TeamList/index.tsx
@@ -1,15 +1,39 @@
 import ProfileItem from '@components/common/ProfileItem';
 import ProfileAddButton from '@components/common/ProfileAddButton';
 import { StTeamListWrapper, StItemContainer } from './style';
+import { Logo } from '@assets/images/index';
 
 function TeamList() {
-  const tempName = '기업적디자인';
+  const tempData = [
+    {
+      id: 1,
+      profileImage: Logo,
+      profileName: 'SOPT',
+    },
+    {
+      id: 2,
+      profileName: '너가소개서',
+    },
+    {
+      id: 3,
+      profileName: '기업적디자인',
+    },
+    {
+      id: 4,
+      profileName: '기업적디자인',
+    },
+    {
+      id: 5,
+      profileName: '기업적디자인',
+    },
+  ];
 
   return (
     <StTeamListWrapper>
       <StItemContainer>
-        <ProfileItem profileName={tempName} />
-        <ProfileItem profileName={tempName} />
+        {tempData.map(({ id, profileImage, profileName }) => (
+          <ProfileItem key={id} profileImage={profileImage} profileName={profileName} />
+        ))}
         <ProfileAddButton />
       </StItemContainer>
     </StTeamListWrapper>

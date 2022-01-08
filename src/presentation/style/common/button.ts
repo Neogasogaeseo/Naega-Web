26324efe845a690c035dcf8d7be3404747e7b76c
jynam <njy1007@gmail.com>
2022-01-08 18:36:02 +0900

    📝 chore: 코랄 메인 색 버튼, 큰 버튼 스타일 상수 지정

---
+++ src/presentation/style/common/button.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+import { COLOR } from '@style/common/color';
+
+export const CORAL_MAIN_BTN = css`
+  background-color: ${COLOR.CORAL_MAIN};
+  color: ${COLOR.WHITE};
+`;
+
+export const BIG_BTN = css`
+  width: 354px;
+  height: 58px;
+  border-radius: 14px;
+  font-size: 16px;
+`;

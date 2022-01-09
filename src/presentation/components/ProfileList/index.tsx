bf1cdf15d9fd28cf6d1d024f148ceccb266a4463
100Gyeon <bjy9966@naver.com>
2022-01-09 18:02:43 +0900

    🔨refactor: 데이터 외부에서 받아오는 형식으로 수정

--- src/presentation/components/ProfileList/index.tsx
+++ src/presentation/components/ProfileList/index.tsx
@@ -1,58 +1,36 @@
 import ProfileItem from '@components/common/ProfileItem';
 import ProfileAddButton from '@components/common/ProfileAddButton';
 import { StProfileListWrapper, StItemContainer } from './style';
-import { Logo } from '@assets/images/index';
+
+interface ProfileListDataProps {
+  id: number;
+  profileImage?: string;
+  profileName: string;
+}
 
 interface ProfileListProps {
   isSquare: boolean;
+  profileListData: ProfileListDataProps[];
   onProfileClick: (id: number) => void;
   onAddClick: () => void;
 }
 
 function ProfileList(props: ProfileListProps) {
-  const { isSquare, onProfileClick, onAddClick } = props;
-  const tempData = [
-    {
-      id: 1,
-      profileImage: Logo,
-      profileName: '너가소개서',
-    },
-    {
-      id: 2,
-      profileName: 'SOPT',
-    },
-    {
-      id: 3,
-      profileName: '기업적디자인',
-    },
-    {
-      id: 4,
-      profileName: '기업적디자인',
-    },
-    {
-      id: 5,
-      profileName: '기업적디자인',
-    },
-    {
-      id: 6,
-      profileName: '기업적디자인',
-    },
-  ];
+  const { isSquare, profileListData, onProfileClick, onAddClick } = props;
 
   return (
     <StProfileListWrapper>
       <StItemContainer isSquare={isSquare}>
-        {tempData &&
-          tempData.map(({ id, profileImage, profileName }) => (
-            <ProfileItem
-              key={id}
-              id={id}
-              profileImage={profileImage}
-              profileName={profileName}
-              isSquare={isSquare}
-              onProfileClick={() => onProfileClick(id)}
-            />
-          ))}
+        {profileListData.map(({ id, profileImage, profileName }) => (
+          <ProfileItem
+            key={id}
+            id={id}
+            profileImage={profileImage}
+            profileName={profileName}
+            isSquare={isSquare}
+            onProfileClick={() => onProfileClick(id)}
+          />
+        ))}
         <ProfileAddButton isSquare={isSquare} onAddClick={onAddClick} />
       </StItemContainer>
     </StProfileListWrapper>
--- src/presentation/pages/Home/Team/index.tsx
+++ src/presentation/pages/Home/Team/index.tsx
@@ -1,8 +1,37 @@
 import ProfileList from '@components/ProfileList';
 import { StTeamMainWrapper, StDivisionLine } from './style';
 import { useNavigate } from 'react-router-dom';
+import { Logo } from '@assets/images/index';
 
 function HomeTeam() {
+  const profileListData = [
+    {
+      id: 1,
+      profileImage: Logo,
+      profileName: '너가소개서',
+    },
+    {
+      id: 2,
+      profileName: 'SOPT',
+    },
+    {
+      id: 3,
+      profileName: '기업적디자인',
+    },
+    {
+      id: 4,
+      profileName: '기업적디자인',
+    },
+    {
+      id: 5,
+      profileName: '기업적디자인',
+    },
+    {
+      id: 6,
+      profileName: '기업적디자인',
+    },
+  ];
+
   const navigate = useNavigate();
 
   const handleProfileClick = (id: number) => {
@@ -19,6 +48,7 @@ function HomeTeam() {
         <h1>나의 팀</h1>
         <ProfileList
           isSquare={true}
+          profileListData={profileListData}
           onProfileClick={handleProfileClick}
           onAddClick={handleAddClick}
         />

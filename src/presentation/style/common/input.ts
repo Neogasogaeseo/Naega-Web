83afc992d3babe9d2be51750263f100d1477c41a
jynam <njy1007@gmail.com>
2022-01-10 04:38:55 +0900

    📝 chore: 공통 input 스타일 상수 지정

---
+++ src/presentation/style/common/input.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+import { COLOR } from '@styles/common/color';
+
+export const COMMON_INPUT = css`
+  border: 1px solid ${COLOR.GRAY_3};
+  border-radius: 16px;
+  font-size: 16px;
+  color: ${COLOR.GRAY_7};
+  &::placeholder {
+    color: ${COLOR.GRAY_5};
+  }
+`;

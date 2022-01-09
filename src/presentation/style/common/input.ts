433b30fbe064a760942c7f0686ecfde444ee9f05
jynam <njy1007@gmail.com>
2022-01-10 04:46:16 +0900

    📝 chore: CommonInput 컴포넌트에 COMMON_INPUT 스타일 상수 적용

--- src/presentation/style/common/input.ts
+++ src/presentation/style/common/input.ts
@@ -2,6 +2,7 @@ import { css } from 'styled-components';
 import { COLOR } from '@styles/common/color';
 
 export const COMMON_INPUT = css`
+  padding: 18px 16px;
   border: 1px solid ${COLOR.GRAY_3};
   border-radius: 16px;
   font-size: 16px;

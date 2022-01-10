b2baeac84ade29a65d247b79b8ce7ff4eedd0133
Hyoin-kim <hi980506@naver.com>
2022-01-11 04:27:23 +0900

    📝chore: 필요한 에셋 추가하기

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as icPlus } from './ic_plus.svg';
 export { default as icCamera } from './ic_camera.svg';
 export { default as icKakao } from './ic_kakao.svg';
+export { default as icEmail } from './ic_email.svg';

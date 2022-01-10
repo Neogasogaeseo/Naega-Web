76ec27637846f54a0db77ff224a39b27e45c078c
Hyoin-kim <hi980506@naver.com>
2022-01-11 04:39:48 +0900

    📝chore: 필요한 에셋 추가하기

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,3 @@
 export { default as Logo } from './logo.svg';
 export { default as imgEmptyProfile } from './img_empty_profile.svg';
+export { default as imgEmptyJoinProfile } from './img_empty_join.svg';

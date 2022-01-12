a3db40b6118f275555f1ee7db2f338324f94f7fd
Hyoin-kim <hi980506@naver.com>
2022-01-12 15:23:48 +0900

    📝chore: Cliend_id 숨기기

--- src/presentation/components/LoginForm/OAuth.ts
+++ src/presentation/components/LoginForm/OAuth.ts
@@ -1,4 +1,4 @@
-const CLIENT_ID = 'd95111016d49b506a9bf9d5dbb1bac9e';
+const CLIENT_ID = `${process.env.REACT_APP_CLIENT_ID}`;
 const REDIRECT_URI = 'http://localhost:3000/oauth';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;

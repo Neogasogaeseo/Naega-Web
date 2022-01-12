5959b8263a66b6b141c71e436343fd4047f65374
100Gyeon <bjy9966@naver.com>
2022-01-13 03:00:31 +0900

    ✨feat: 이미지 배열 렌더링

--- src/presentation/components/common/IssueMemberList/index.tsx
+++ src/presentation/components/common/IssueMemberList/index.tsx
@@ -1,12 +1,23 @@
 import { StIssueMemberList } from './style';
-import { imgLogo } from '@assets/images';
 
-function IssueMemberList() {
+interface IssueMemberListProps {
+  id: number;
+  issueMembers: string[];
+}
+
+function IssueMemberList(props: IssueMemberListProps) {
+  const { id, issueMembers } = props;
+  const length = issueMembers.length;
+  const MAX_IMAGE_NUM = 3;
+
   return (
     <StIssueMemberList>
-      <img src={imgLogo} />
-      <img src={imgLogo} />
-      <img src={imgLogo} />
+      <div>
+        {issueMembers.slice(0, MAX_IMAGE_NUM).map((member, index) => (
+          <img key={`member-${id}-${index}`} src={member} />
+        ))}
+      </div>
+      {length <= MAX_IMAGE_NUM ? null : <span>+{length - MAX_IMAGE_NUM}</span>}
     </StIssueMemberList>
   );
 }

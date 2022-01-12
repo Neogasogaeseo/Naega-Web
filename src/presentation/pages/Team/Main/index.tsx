ee88dd154e7bc8cbc4d0924dc6a440c97de2f885
100Gyeon <bjy9966@naver.com>
2022-01-12 16:33:19 +0900

    📝chore: 이슈 카드 필요한 부분에 추가

--- src/presentation/pages/Team/Main/index.tsx
+++ src/presentation/pages/Team/Main/index.tsx
@@ -1,8 +1,38 @@
+import { imgLogo } from '@assets/images';
+import IssueCardList from '@components/common/IssueCardList';
 import { useParams } from 'react-router-dom';
+import { StTeamMain } from './style';
 
 function TeamMain() {
   const { teamID } = useParams();
-  return <div>팀원소개서 메인, 팀 번호는 {teamID}</div>;
+  const issueListData = [
+    {
+      id: 1,
+      category: '팀컬쳐',
+      dates: '2021-12-27',
+      content:
+        '깃알못이라 IOS 프로젝트가 엉켜서 망가졌다 깃알못이라 IOS 프로젝트가 엉켜서 망가졌다',
+      teamImage: imgLogo,
+      teamName: '너가소개서',
+      memberName: '강쥐',
+    },
+    {
+      id: 2,
+      category: '개발',
+      dates: '2021-12-27',
+      content:
+        '깃알못이라 IOS 프로젝트가 엉켜서 망가졌다 깃알못이라 IOS 프로젝트가 엉켜서 망가졌다',
+      teamName: '너가소개서',
+      memberName: '강쥐',
+    },
+  ];
+
+  return (
+    <StTeamMain>
+      팀원소개서 메인, 팀 번호는 {teamID}
+      <IssueCardList issueListData={issueListData} />
+    </StTeamMain>
+  );
 }
 
 export default TeamMain;

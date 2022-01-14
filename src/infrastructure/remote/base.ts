1e1a08a80ad814447db0316e32fe764e030075d3
seojin kim <001106ksj@gmail.com>
2022-01-14 18:35:16 +0900

    ✨feat: 베이스 API 쓰기

---
+++ src/infrastructure/remote/base.ts
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+const BASEURL = '추가예정';
+const getAccessToken = () => localStorage.getItem('token') ?? '';
+
+const basePrivateHeaders = {
+  Accept: `*/*`,
+  'Content-Type': `application/json`,
+  accesstoken: getAccessToken(),
+};
+
+const basePublicHeaders = {
+  Accept: `*/*`,
+  'Content-Type': `application/json`,
+};
+
+interface Request {
+  url: string;
+  headers?: object;
+  isPrivate: boolean;
+  method: 'get' | 'post' | 'put' | 'delete';
+}
+
+interface RequestWithParams extends Request {
+  params?: object;
+}
+
+interface RequestWithData extends Request {
+  data?: object;
+}
+
+const sendRequest = ({ url, params, method, headers, isPrivate }: RequestWithParams) => {
+  const baseHeaders = isPrivate ? basePrivateHeaders : basePublicHeaders;
+  return axios[method](BASEURL + url, {
+    headers: { ...baseHeaders, ...headers },
+    params,
+  }).then((response) => {
+    return response.data;
+  });
+};
+
+const sendRequestForData = ({ url, data, method, headers, isPrivate }: RequestWithData) => {
+  const baseHeaders = isPrivate ? basePrivateHeaders : basePublicHeaders;
+  return axios[method](BASEURL + url, data, {
+    headers: { ...baseHeaders, ...headers },
+  }).then((response) => {
+    return response.data;
+  });
+};
+
+export const privateAPI = {
+  get: ({ url, params, headers }: { url: string; params?: object; headers?: object }) =>
+    sendRequest({ url, params, method: 'get', headers, isPrivate: true }),
+  post: ({ url, data, headers }: { url: string; data?: object; headers?: object }) =>
+    sendRequestForData({ url, data, method: 'post', headers, isPrivate: true }),
+  put: ({ url, data, headers }: { url: string; data?: object; headers?: object }) =>
+    sendRequestForData({ url, data, method: 'put', headers, isPrivate: true }),
+  delete: ({ url, params, headers }: { url: string; params?: object; headers?: object }) =>
+    sendRequest({ url, params, method: 'delete', headers, isPrivate: true }),
+};
+
+export const publicAPI = {
+  get: ({ url, params, headers }: { url: string; params?: object; headers?: object }) =>
+    sendRequest({ url, params, method: 'get', headers, isPrivate: false }),
+  post: ({ url, data, headers }: { url: string; data?: object; headers?: object }) =>
+    sendRequestForData({ url, data, method: 'post', headers, isPrivate: false }),
+  put: ({ url, data, headers }: { url: string; data?: object; headers?: object }) =>
+    sendRequestForData({ url, data, method: 'put', headers, isPrivate: false }),
+  delete: ({ url, params, headers }: { url: string; params?: object; headers?: object }) =>
+    sendRequest({ url, params, method: 'delete', headers, isPrivate: false }),
+};

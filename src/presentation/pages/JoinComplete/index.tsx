463d9895abaf96b19733534a3cd3a3d77803528b
Hyoin-kim <hi980506@naver.com>
2022-01-14 19:48:34 +0900

    ✨feat: 회원가입 완료페이지 퍼블리싱 구현

---
+++ src/presentation/pages/JoinComplete/index.tsx
@@ -0,0 +1,11 @@
+import JoinCompleteForm from '@components/JoinCompleteForm';
+
+function joinComplete() {
+  return (
+    <div>
+      <JoinCompleteForm />
+    </div>
+  );
+}
+
+export default joinComplete;

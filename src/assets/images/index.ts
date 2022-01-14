f5c89f6fbfc1caac213afe53554ea41a94933949
Hyoin-kim <hi980506@naver.com>
2022-01-14 19:49:12 +0900

    📝chore: 필요한 에셋 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,5 @@
 export { default as imgLogo } from './img_logo.svg';
 export { default as imgEmptyProfile } from './img_empty_profile.svg';
 export { default as imgEmptyJoinProfile } from './img_empty_join.svg';
+export { default as imgParty } from './img_party.svg';
 export { ReactComponent as ImgTeamAdd } from './img_team_add.svg';

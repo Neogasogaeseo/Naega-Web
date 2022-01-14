c4cfa5467adf2ee5b547f008f1e6e061fb4e5b24
Hyoin-kim <hi980506@naver.com>
2022-01-14 19:49:48 +0900

    📝chore: 너가소개서 결과페이지 라우트 지정

--- src/presentation/routes/NeogaRouter.tsx
+++ src/presentation/routes/NeogaRouter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NeogaCreate from '@pages/Neoga/Create';
+import FormDetail from '@pages/Neoga/FormDetail';
 
 function NeogaRouter() {
   return (
     <Routes>
       <Route path="/create" element={<NeogaCreate />} />
+      <Route path="/detail/form" element={<FormDetail />} />
     </Routes>
   );
 }

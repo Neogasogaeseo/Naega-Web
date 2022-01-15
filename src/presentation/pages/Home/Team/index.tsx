b69e107ba78789585a24b23dc10f8f1efbac2237
100Gyeon <bjy9966@naver.com>
2022-01-15 19:50:49 +0900

    🎨design: 팀 초대 관련 구조화 및 스타일링

--- src/presentation/pages/Home/Team/index.tsx
+++ src/presentation/pages/Home/Team/index.tsx
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { api } from '@api/index';
 import ProfileList from '@components/ProfileList';
 import IssueCardList from '@components/common/IssueCardList';
-import { StTeamMain, StDivisionLine } from './style';
+import { StTeamMain, StInvitation, StDivisionLine } from './style';
 import { TeamIssueCard, TeamMember } from '@api/types/team';
+import { icMessage } from '@assets/icons';
 
 function HomeTeam() {
   const [profileListData, setProfileListData] = useState<TeamMember[] | null>(null);
   const [issueListData, setIssueListData] = useState<TeamIssueCard[] | null>(null);
   const [isValidating, setIsValidating] = useState(false);
+  const [isInviting, setIsInviting] = useState(true); // false로 변경
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -30,36 +32,45 @@ function HomeTeam() {
     };
   }, []);
 
-  const handleProfileClick = (id: string) => {
-    navigate(`/team/${id}`);
-  };
-
-  const handleAddClick = () => {
-    navigate('/team/register');
-  };
-
-  const handleIssueClick = (teamID: string, issueNumber: number) => {
-    navigate(`/team/${teamID}/${issueNumber}`);
-  };
-
   return (
     <>
       <StTeamMain>
+        {isInviting && (
+          <StInvitation>
+            <div>
+              <img src={icMessage} />
+              <span>너가소개서너가소개서팀</span>의 초대
+            </div>
+            <div>
+              <button onClick={() => setIsInviting(true)}>수락</button>
+              <button onClick={() => setIsInviting(false)}>거절</button>
+            </div>
+          </StInvitation>
+        )}
         <h1>나의 팀</h1>
         {isValidating && <div>로딩중</div>}
         {profileListData && (
           <ProfileList
             isSquare={true}
             profileListData={profileListData}
-            onProfileClick={handleProfileClick}
-            onAddClick={handleAddClick}
+            onProfileClick={(id) => {
+              navigate(`/team/${id}`);
+            }}
+            onAddClick={() => {
+              navigate('/team/register');
+            }}
           />
         )}
         <StDivisionLine />
         <h1>나와 관련된 이슈 확인</h1>
         {isValidating && <div>로딩중</div>}
         {issueListData && (
-          <IssueCardList issueListData={issueListData} onIssueClick={handleIssueClick} />
+          <IssueCardList
+            issueListData={issueListData}
+            onIssueClick={(teamID, issueNumber) => {
+              navigate(`/team/${teamID}/${issueNumber}`);
+            }}
+          />
         )}
       </StTeamMain>
     </>
--- src/presentation/pages/Home/Team/style.ts
+++ src/presentation/pages/Home/Team/style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { COLOR } from '@styles/common/color';
+import { FONT_STYLES } from '@styles/common/font-style';
 
 export const StTeamMain = styled.div`
   & h1 {
@@ -8,6 +9,47 @@ export const StTeamMain = styled.div`
     color: ${COLOR.GRAY_8};
     margin-top: 28px;
     margin-bottom: 18px;
+  }
+`;
+
+export const StInvitation = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${COLOR.GRAY_1};
+  color: ${COLOR.GRAY_6};
+  width: calc(100% + 2 * 20px);
+  height: 57px;
+  margin-left: -20px;
+  ${FONT_STYLES.R_14_TITLE};
+  padding-left: 14px;
+  padding-right: 19px;
+
+  & div {
+    display: flex;
+    align-items: center;
+  }
+
+  span {
+    color: ${COLOR.CORAL_MAIN};
+    font-weight: 600;
+  }
+
+  button {
+    border-radius: 8px;
+    padding: 10px 14px;
+    ${FONT_STYLES.M_13_TITLE};
+  }
+
+  button:nth-of-type(1) {
+    margin-right: 6px;
+    color: ${COLOR.WHITE};
+    background: ${COLOR.CORAL_MAIN};
+  }
+
+  button:nth-of-type(2) {
+    color: ${COLOR.GRAY_6};
+    background-color: ${COLOR.GRAY_3};
   }
 `;
 

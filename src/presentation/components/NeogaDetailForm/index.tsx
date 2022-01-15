6d9cd9d051288a124fd66ef5aea76d59e3679106
Hyoin-Kim <hi980506@naver.com>
2022-01-15 22:17:26 +0900

    ✨feat: 너가소개서 디테일 폼 결과보기 구현중

--- src/presentation/components/NeogaDetailForm/index.tsx
+++ src/presentation/components/NeogaDetailForm/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import ImmutableKeywordList from '@components/common/Keyword/ImmutableList';
+import {icLink} from '@assets/icons/index';
 import {
   StNeogaDetailForm,
   StTitle,
@@ -12,6 +13,9 @@ import {
   StFeedNickname,
   StFeedContent,
   StFeedTitle,
+  StQuestion,
+  StIcon,
+  StLink
 } from './style';
 
 interface Keyword {
@@ -35,16 +39,21 @@ function NeogaDetailForm() {
           2022, 임인년 <br />
           강쥐에게 기대하는 모습은?
         </StTitle>
-        <StDate>{date}</StDate>
+        <StIcon></StIcon>
       </StHeader>
+      <StLink><img src={icLink}/>링크 복사하기</StLink>
+      <StDate>{date}에 생성</StDate>
+      <StQuestion><p>Q.</p>나와 함께하며 당신이 닮고 싶었던 능력이 있었을까요 ?
+      </StQuestion>
       <StKeyword>
         <p>키워드모음</p>
       </StKeyword>
+      <hr />
       <StFeedWrapper>
         <StFeedTitle>
           <p>5개</p>의 답변 피드
         </StFeedTitle>
-        <StFeedNickname>동네친구 수진</StFeedNickname>
+        <StFeedNickname>동네친구 <p>수진</p></StFeedNickname>
         <StFeedContent>
           강쥐야 너랑 작업같이 진행하면서 너가 있어서 넘 든든했어! 우리 앞으로도 꼭 같이 머머하자
         </StFeedContent>
@@ -54,7 +63,6 @@ function NeogaDetailForm() {
             return;
           }}
         />
-        <hr />
       </StFeedWrapper>
       <StButton type="submit">링크 복사하기</StButton>
     </StNeogaDetailForm>
--- src/presentation/components/NeogaDetailForm/style.ts
+++ src/presentation/components/NeogaDetailForm/style.ts
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 import { COLOR } from '@styles/common/color';
-export const StNeogaDetailForm = styled.div``;
+export const StNeogaDetailForm = styled.div`
+& > hr {
+  border: 1px solid ${COLOR.GRAY_2};
+}`;
+export const StLink = styled.div`
+  margin-left: 20px;
+  margin-top: 18px;
+  color: ${COLOR.CORAL_MAIN};
+  &>img{
+    padding-right:5px;
+  }
+`;
+
 export const StHeader = styled.div`
   display: flex;
   justify-content: space-between;
@@ -8,10 +20,37 @@ export const StHeader = styled.div`
 `;
 export const StDate = styled.div`
   display: flex;
+  float: right;
   font-size: 13px;
   line-height: 100%;
-  margin-top: 5px;
+  margin: 5px 20px 9px 0px;
   color: ${COLOR.GRAY_5};
+`;
+
+export const StQuestion = styled.div`
+&> p{
+  color: ${COLOR.CORAL_MAIN};
+  padding-right: 12px;
+  font-size: 15px;
+  display:flex;
+}
+  display:flex;
+  background-color: ${COLOR.GRAY_1};
+  width:350px;
+  margin-left: 12px;
+  border-radius: 18px;
+  padding: 16px 19px 16px 20px;
+  font-size: 15px;
+  line-height: 143.99%;
+  color: ${COLOR.GRAY_7};
+`;
+
+export const StIcon = styled.div`
+width: 72px;
+height: 72px;
+border-radius: 80px;
+background-color: ${COLOR.GRAY_1};
+
 `;
 
 export const StTitle = styled.div`
@@ -42,11 +81,9 @@ export const StButton = styled.button`
 `;
 export const StKeyword = styled.div`
   width: 100%;
-  height: 164px;
   padding: 20px;
-  margin-top: 33px;
-  background: ${COLOR.GRAY_1};
   & > p {
+    margin-top: 12px;
     font-weight: 600;
     font-size: 16px;
     line-height: 162%;
@@ -57,16 +94,26 @@ export const StKeyword = styled.div`
 
 export const StFeedWrapper = styled.div`
   margin: 0 20px 0 20px;
-  & > hr {
-    border: 1px solid ${COLOR.GRAY_2};
-  }
 `;
 
 export const StFeedNickname = styled.div`
+  display:flex;  
   margin-bottom: 12px;
+  font-size: 14px;
+  line-height: 140%;
+  letter-spacing: -0.01em;
+  color: ${COLOR.GRAY_7};
 `;
 export const StFeedContent = styled.div``;
 export const StFeedTitle = styled.div`
+  display: flex;
   margin-top: 20px;
   margin-bottom: 23px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 100%;
+  letter-spacing: -0.01em;
+  &>p{
+    color: ${COLOR.CORAL_MAIN};
+  }
 `;

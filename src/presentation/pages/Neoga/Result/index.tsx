469850bdab580a921e269be8fcf0c48f997c9c44
100Gyeon <bjy9966@naver.com>
2022-01-17 01:44:54 +0900

    🎨design: 만든 너소서 페이지 구조화 및 스타일링

---
+++ src/presentation/pages/Neoga/Result/index.tsx
@@ -0,0 +1,20 @@
+import { imgLogo } from '@assets/images';
+import NeogaResultCard from '@components/common/NeogaResultCard';
+import { Link } from 'react-router-dom';
+import { StNeogaResult } from './style';
+
+function NeogaResult() {
+  return (
+    <StNeogaResult>
+      <Link to="/home">
+        <img src={imgLogo} />
+      </Link>
+      <h1>지연님이 만든 너가소개서</h1>
+      <h2>내가 생성한 너가소개서를 확인하세요</h2>
+      <NeogaResultCard />
+      <NeogaResultCard />
+    </StNeogaResult>
+  );
+}
+
+export default NeogaResult;

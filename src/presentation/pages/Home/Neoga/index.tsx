d7a54c8c7b5a3a59143638b718a85528d436ee0b
100Gyeon <bjy9966@naver.com>
2022-01-17 01:43:01 +0900

    ✨feat: 너소서 특정 페이지로 이동하도록 기능, 라우터 추가

--- src/presentation/pages/Home/Neoga/index.tsx
+++ src/presentation/pages/Home/Neoga/index.tsx
@@ -1,8 +1,11 @@
 import NeogaResultCard from '@components/common/NeogaResultCard';
 import { StBanner, StForm, StHomeNeoga, StResult } from './styles';
 import { icWhole } from '@assets/icons';
+import { useNavigate } from 'react-router-dom';
 
 function HomeNeoga() {
+  const navigate = useNavigate();
+
   return (
     <StHomeNeoga>
       <StBanner>배너</StBanner>
@@ -10,7 +13,7 @@ function HomeNeoga() {
         <h1>너가소개서 설문 만들기</h1>
         <div>
           <h2>지인에게 궁금한 내 모습을 물어보세요</h2>
-          <button>
+          <button onClick={() => navigate('/neoga/create')}>
             전체보기
             <img src={icWhole} />
           </button>
@@ -20,11 +23,12 @@ function HomeNeoga() {
         <h1>지연님이 만든 너가소개서</h1>
         <div>
           <h2>내가 생성한 너가소개서를 확인하세요!</h2>
-          <button>
+          <button onClick={() => navigate('/neoga/result')}>
             전체보기
             <img src={icWhole} />
           </button>
         </div>
+        <NeogaResultCard />
         <NeogaResultCard />
       </StResult>
     </StHomeNeoga>
--- src/presentation/routes/NeogaRouter.tsx
+++ src/presentation/routes/NeogaRouter.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NeogaCreate from '@pages/Neoga/Create';
+import NeogaResult from '@pages/Neoga/Result';
 
 function NeogaRouter() {
   return (
     <Routes>
       <Route path="/create" element={<NeogaCreate />} />
+      <Route path="/result" element={<NeogaResult />} />
     </Routes>
   );
 }

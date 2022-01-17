fdb3fed5ff59d201caa4e0c29a339c33c65d6eda
Hyoin-Kim <hi980506@naver.com>
2022-01-18 04:43:19 +0900

    ✨feat: 랜딩페이지 구현중

--- src/presentation/components/LandingPage/index.tsx
+++ src/presentation/components/LandingPage/index.tsx
@@ -1,9 +1,53 @@
-import {StLandingPage} from './style';
+import {imgLogo,ImgMainPage,ImgMainPhone} from '@assets/images';
+import { useNavigate } from 'react-router-dom';
+import {
+    StLandingPage,
+    StLogoImg,
+    StLogin,
+    StHeader,
+    StMain,
+    StContent,
+    StMainImg,
+    StServiceButton,
+    StMiddle,
+    StMiddleTitle,
+    StMiddleContent,
+} from './style';
 
 function LandingPage(){
+    const navigate = useNavigate();
     return (
         <StLandingPage>
-            안녕하세요
+            <StHeader>
+                <StLogoImg src={imgLogo}></StLogoImg>
+                <StLogin onClick={()=>{
+                    navigate(`/login`);
+                }}>로그인</StLogin>
+            </StHeader>
+            <StMain>
+                <p>너가 나좀..소개해줄래?</p>
+                <h2>너를 통해 발견하는 나</h2>
+                <StContent>내가 나를 말하기는 낯 간지럽고<br/>
+                누군가 이렇게 괜찮은 나를 설명해줬음 할 때<br/>
+                모두 너가소개서 하세요!
+                </StContent>
+                <StMainImg src={ImgMainPage} />
+                <StServiceButton onClick={()=>{
+                    navigate(`/login`);
+                }}>서비스 시작하기</StServiceButton>
+            </StMain>
+            <StMiddle>
+                <StMiddleTitle>
+                <h2>나를 알아갈 수 있는<br/>
+                다양한 질문, 너가소개서</h2>
+                </StMiddleTitle>
+                <StMiddleContent>
+                    주변에 궁금했던 설문을 골라 <br/>
+                    친구들의 답변으로 새로운 나를 발견하세요
+                </StMiddleContent>
+                {/* <StMiddleImg src={ImgMainPhone}></StMiddleImg> */}
+                <ImgMainPhone />
+            </StMiddle>
         </StLandingPage>
     );
 };
--- src/presentation/components/LandingPage/style.ts
+++ src/presentation/components/LandingPage/style.ts
@@ -1,3 +1,84 @@
 import styled from 'styled-components';
+import {COLOR} from '@styles/common/color';
 
 export const StLandingPage = styled.div``;
+export const StHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 16px 48px 20px;
+    align-items: center;
+`;
+export const StLogoImg = styled.img``;
+export const StLogin = styled.button`
+    width: 58px;
+    height: 27px;
+    background-color: ${COLOR.CORAL_MAIN};
+    color: white;
+    border-radius: 13.5px;
+`;
+export const StMainImg = styled.img``;
+export const StMain = styled.div`
+    text-align: center;
+    letter-spacing: -0.01em;
+&>p{
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 162%;
+    color: ${COLOR.CORAL_MAIN};
+}
+&>h2{
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 143.99%;
+    letter-spacing: -0.01em;
+    color: ${COLOR.GRAY_8};
+}
+`;
+export const StContent = styled.div`
+    font-size: 15px;
+    line-height: 100%;
+    text-align: center;
+    line-height: 21px;
+    letter-spacing: -0.01em;
+    color: ${COLOR.GRAY_6};
+    margin: 17px 0px 56px 0px;
+`;
+
+export const StServiceButton = styled.button`
+    width: 202px;
+    height: 52px;
+    display: block;
+    background-color: ${COLOR.CORAL_MAIN};
+    color: white;
+    border-radius: 18px;
+    margin: 57.6px 0px 0px 90px;
+`;
+
+export const StMiddle = styled.div`
+    width: 100%;
+    height: 852px;
+    background-color: ${COLOR.GRAY_1};
+    margin-top: 69px;
+`;
+
+export const StMiddleTitle = styled.div`
+&>h2{
+    margin: 70px 0px 20px 0px;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+    text-align: center;
+    letter-spacing: -0.01em;
+    color: ${COLOR.GRAY_8};
+}
+`;
+export const StMiddleContent = styled.div`
+    font-size: 15px;
+    line-height: 100%;
+    text-align: center;
+    letter-spacing: -0.01em;
+    color: ${COLOR.GRAY_5};
+    margin-bottom: 22px;
+`;
+export const StMiddleImg = styled.img``;
+

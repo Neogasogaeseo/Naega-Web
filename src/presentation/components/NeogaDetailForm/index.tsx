e6ce57d16f9e68591ef68e0a9738d53c834afbc6
Hyoin-Kim <hi980506@naver.com>
2022-01-17 18:15:17 +0900

    ✨feat: 키워드 더보기/접기 기능 구현

--- src/presentation/components/NeogaDetailForm/index.tsx
+++ src/presentation/components/NeogaDetailForm/index.tsx
@@ -1,85 +1,133 @@
 import { api } from '@api/index';
 import { useEffect, useState } from 'react';
 import ImmutableKeywordList from '@components/common/Keyword/ImmutableList';
-import {icLink} from '@assets/icons/index';
+import { icLink, IcArrowDown,IcArrowUp } from '@assets/icons/index';
 import {
   StNeogaDetailForm,
   StTitle,
-  StButton,
   StDate,
   StHeader,
   StKeyword,
   StFeedWrapper,
-  StFeedNickname,
+  StFeedHeader,
   StFeedContent,
   StFeedTitle,
   StQuestion,
   StLink,
   StFeedName,
   StFeedDate,
+  StEmptyFeedback,
+  StButton,
+  StMoreWrapper,StMoreButton,
 } from './style';
 import { Keywordlists, ResultFormList } from '@api/types/neoga';
-
+import { imgEmptyFeedback } from '@assets/images';
 
 function NeogaDetailForm() {
-  const [resultKeywordList,setResultKeywordList] = useState<Keywordlists[]>([]);
+  const [resultKeywordList, setResultKeywordList] = useState<Keywordlists[]>([]);
   const [resultList, setResultList] = useState<ResultFormList[]>([]);
+  const [resultBoolean, setResultBoolean] = useState(false);
+  const [lookMoreButton, setLookMoreButton] = useState(false);
 
-    useEffect(()=>{
-      (async ()=>{
-        const data= await api.neogaService.getResultTemplates();
-        setResultKeywordList(data);
-        console.log(resultKeywordList.length);
-      })();
-    },[resultKeywordList]);
+  useEffect(()=>{
+    (async ()=>{
+      const data= await api.neogaService.getResultTemplates();
+      setResultBoolean(true);
+      setResultKeywordList(data);
+    })();
+  },[resultKeywordList]);
 
-    useEffect(()=>{
-      (async ()=>{
-        const data= await api.neogaService.getAllResultListTemplates();
-        setResultList(data);
-      })();
-    },[resultList]);
+  useEffect(()=>{
+    (async ()=>{
+      const data= await api.neogaService.getAllResultListTemplates();
+      setResultBoolean(true);
+      setResultList(data);
+    })();
+  },[resultList]);
+
+  const onClickMore = ()=>{
+    setLookMoreButton(true);
+  }
+
+  const onClickFold = ()=>{
+    setLookMoreButton(false);
+  }
 
   return (
     <StNeogaDetailForm>
       <div>
-      <StHeader>
-        <StTitle>
-          2022, 임인년 <br />
-          강쥐에게 기대하는 모습은?
-        </StTitle>
-      </StHeader>
-      <StLink><img src={icLink}/><p>링크 복사하기</p></StLink>
-      <StDate>2021-01-17 에 생성</StDate>
-      <StQuestion><p>Q.</p>나와 함께하며 당신이 닮고 싶었던 능력이 있었을까요 ?
-      </StQuestion>
+        <StHeader>
+          <StTitle>
+            2022, 임인년 <br />
+            강쥐에게 기대하는 모습은?
+          </StTitle>
+        </StHeader>
+        <StLink>
+          <img src={icLink} />
+          <p>링크 복사하기</p>
+        </StLink>
+        <StDate>2021-01-17 에 생성</StDate>
+        <StQuestion>
+          <span>Q.</span>나와 함께하며 당신이 닮고 싶었던 능력이 있었을까요 ?
+        </StQuestion>
       </div>
-      <StKeyword>
-        <p>키워드모음</p>
-        <ImmutableKeywordList keywordList={resultKeywordList} onItemClick={() => null} />
-      </StKeyword>
-      <hr />
-      <StFeedTitle>
-          <p>5개</p>의 답변 피드
-      </StFeedTitle>
-      {
-        resultList.map((data)=>{
-          return(
-            <>
-            <StFeedWrapper>
-            <StFeedNickname><StFeedName>{data.category}·<p>{data.writer}</p></StFeedName>
-            <StFeedDate>{data.createdAt}</StFeedDate></StFeedNickname>
-            <StFeedContent>
-              {data.content}
-            </StFeedContent>
-            <ImmutableKeywordList keywordList={resultKeywordList} onItemClick={() => null} />
-            <hr/>
-          </StFeedWrapper>
-          </>
-          )
-        })
-      }
-      <StButton type="submit">링크 복사하기</StButton>
+      {resultBoolean ? (
+        <>
+          <StKeyword>
+            <p>키워드모음</p>
+            {
+              !lookMoreButton ? (
+                <ImmutableKeywordList keywordList={resultKeywordList.slice(0,7)} onItemClick={() => null} />
+              ):null
+            }
+            <StMoreWrapper>
+              {
+                lookMoreButton && resultKeywordList.length > 7 ? (
+                <>
+                  <ImmutableKeywordList keywordList={resultKeywordList} onItemClick={() => null} />
+                  <hr/>
+                  <StMoreButton onClick={onClickFold}>접기<img src={IcArrowUp}></img></StMoreButton>
+                </>
+                )
+                : resultKeywordList.length > 7 ? (
+                <>
+                  <hr/>
+                  <StMoreButton onClick={onClickMore}>더보기<img src={IcArrowDown}></img></StMoreButton>
+                </>
+                ) : null
+              }
+            </StMoreWrapper>
+          </StKeyword>
+          <hr />
+          <StFeedTitle>
+            <span>5개</span>의 답변 피드
+          </StFeedTitle>
+          {resultList.map((data) => {
+            return (
+              <>
+                <StFeedWrapper>
+                  <StFeedHeader>
+                    <StFeedName>
+                      {data.category}·<span>{data.writer}</span>
+                    </StFeedName>
+                    <StFeedDate>{data.createdAt}</StFeedDate>
+                  </StFeedHeader>
+                  <StFeedContent>{data.content}</StFeedContent>
+                  <ImmutableKeywordList keywordList={resultKeywordList} onItemClick={() => null} />
+                  <hr />
+                </StFeedWrapper>
+              </>
+            );
+          })}
+        </>
+      ) : (
+        <>
+        <StEmptyFeedback>
+          <img src={imgEmptyFeedback} alt="" />
+          <StButton>링크 복사하기</StButton>
+        </StEmptyFeedback>
+        </>
+      )}
     </StNeogaDetailForm>
   );
 }
--- src/presentation/components/NeogaDetailForm/style.ts
+++ src/presentation/components/NeogaDetailForm/style.ts
@@ -2,35 +2,41 @@ import styled from 'styled-components';
 import { COLOR } from '@styles/common/color';
 
 export const StNeogaDetailForm = styled.div`
-& > hr {
-  border: 1px solid ${COLOR.GRAY_2};
-  height: 8px;
-  background-color: ${COLOR.GRAY_1};
-}`;
+  & > hr {
+    border: 1px solid ${COLOR.GRAY_2};
+    height: 8px;
+    background-color: ${COLOR.GRAY_1};
+  }
+`;
 
 export const StLink = styled.div`
   display: flex;
+  cursor: pointer;
   margin: 18px 0px 0px 20px;
   color: ${COLOR.CORAL_MAIN};
-  &>img{
-    padding-right:5px;
+  & > img {
+    padding-right: 5px;
+  }
+  & > p {
+    margin-top: 3px;
   }
 `;
 
 export const StFeedName = styled.div`
-font-weight: 600;
-font-size: 13px;
-line-height: 100%;
-display: flex;
-align-items: center;
-letter-spacing: -0.01em;
-color: ${COLOR.GRAY_6};
-
-&>p{
-  font-weight: normal;
+  font-weight: 600;
   font-size: 13px;
-  color: ${COLOR.GRAY_5};
-}`;
+  line-height: 100%;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.01em;
+  color: ${COLOR.GRAY_6};
+
+  & > span {
+    font-weight: normal;
+    font-size: 13px;
+    color: ${COLOR.GRAY_5};
+  }
+`;
 
 export const StHeader = styled.div`
   display: flex;
@@ -48,23 +54,22 @@ export const StDate = styled.div`
 `;
 
 export const StQuestion = styled.div`
-&> p{
-  color: ${COLOR.CORAL_MAIN};
-  padding-right: 12px;
-  font-size: 15px;
-  display:flex;
-}
-  display:flex;
+  & > span {
+    color: ${COLOR.CORAL_MAIN};
+    padding-right: 12px;
+    font-size: 15px;
+    display: flex;
+  }
+  display: flex;
   background-color: ${COLOR.GRAY_1};
-  width: 100%;
-  margin-left: 12px;
+  width: 370px;
+  margin: 0px 12px 0px 12px;
   border-radius: 18px;
   padding: 16px 19px 16px 20px;
   font-size: 15px;
   line-height: 143.99%;
   color: ${COLOR.GRAY_7};
 `;
-
 
 export const StTitle = styled.div`
   margin: 50px 20px 0 20px;
@@ -78,19 +83,6 @@ export const StTitle = styled.div`
     line-height: 162%;
     letter-spacing: -0.01em;
   }
-`;
-
-export const StButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 70px 0px 0px 20px;
-  width: 350px;
-  height: 58px;
-  background-color: ${COLOR.CORAL_MAIN};
-  color: white;
-  border-radius: 18px;
-  font-size: 16px;
 `;
 
 export const StKeyword = styled.div`
@@ -112,25 +104,24 @@ export const StFeedDate = styled.div`
 
 export const StFeedWrapper = styled.div`
   margin: 0 20px 0 20px;
-  & >hr{
-    margin-top:24px;
-    border: 1px solid #EFEFEF;
+  & > hr {
+    margin-top: 24px;
+    border: 1px solid #efefef;
     border-radius: 2px;
   }
 `;
 
-export const StFeedNickname = styled.div`
-  display:flex;  
+export const StFeedHeader = styled.div`
+  display: flex;
   justify-content: space-between;
   margin: 24px 0px 12px 0px;
   font-size: 14px;
   line-height: 140%;
   letter-spacing: -0.01em;
   color: ${COLOR.GRAY_7};
-
 `;
 export const StFeedContent = styled.div`
-  margin-bottom:14px;
+  margin-bottom: 14px;
 `;
 
 export const StFeedTitle = styled.div`
@@ -140,7 +131,40 @@ export const StFeedTitle = styled.div`
   font-size: 16px;
   line-height: 100%;
   letter-spacing: -0.01em;
-  &>p{
+  & > span {
     color: ${COLOR.CORAL_MAIN};
   }
+`;
+
+export const StEmptyFeedback = styled.div`
+  display: grid;
+  place-content: center;
+  margin-top: 127px;
+`;
+
+export const StButton = styled.button`
+  align-items: center;
+  justify-content: center;
+  margin-top: 47px;
+  width: 156px;
+  height: 52px;
+  background-color: ${COLOR.CORAL_MAIN};
+  color: white;
+  border-radius: 18px;
+  font-size: 16px;
+`;
+
+export const StMoreWrapper = styled.div`
+& > hr{
+  margin-top: 32px;
+  border: 1px solid ${COLOR.GRAY_2};
+  border-radius: 2px; 
+}
+`;
+export const StMoreButton = styled.div`
+color: ${COLOR.GRAY_4};
+cursor: pointer;
+display:flex;
+justify-content: center;
+margin-top: 18px;
 `;

8f4e2bd2897aa26cfcd0d12e4ee8bdf3463c54d9
Hyoin-Kim <hi980506@naver.com>
2022-01-18 19:47:05 +0900

    ✨feat: 서버 연결중

--- src/infrastructure/api/login-user.ts
+++ src/infrastructure/api/login-user.ts
@@ -1,5 +1,20 @@
-import { LoginUser } from './types/user';
+import { LoginUser, IJoin } from './types/user';
+import { publicAPI } from '../remote/base';
 
 export interface LoginUserService {
   getUserInfo(token: string): Promise<LoginUser>;
+}
+
+export const postJoin = async (joinData: IJoin) => {
+  try {
+    const response = await publicAPI.post({url:`/auth/register`,data: joinData});
+    console.log("대답말해라",response);
+    if(response.status === 200){
+      console.log(response.data);
+    }
+  }catch (e){
+    console.log("잘못됨",e);
+
+  }
+
 }

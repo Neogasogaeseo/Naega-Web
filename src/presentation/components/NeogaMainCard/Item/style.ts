b8f1d7940e318b011a2001ad8eea296bde1a6574
100Gyeon <bjy9966@naver.com>
2022-01-18 18:35:47 +0900

    🎨design: 너가소개서 설문 만들기 구조화 및 스타일링

---
+++ src/presentation/components/NeogaMainCard/Item/style.ts
@@ -0,0 +1,29 @@
+import { COLOR } from '@styles/common/color';
+import { FONT_STYLES } from '@styles/common/font-style';
+import styled from 'styled-components';
+
+export const StNeogaMainCardItem = styled.div`
+  background-color: ${(props) => props.color};
+  ${FONT_STYLES.M_15_TITLE};
+  border-radius: 18.1233px;
+  color: ${COLOR.WHITE};
+  width: 136px;
+  height: 156px;
+  padding: 14px 16px 20px 8px;
+  word-break: keep-all;
+
+  & + & {
+    margin-left: 8px;
+  }
+
+  & img {
+    width: 64px;
+    height: 64px;
+    object-fit: cover;
+    margin-bottom: 21px;
+  }
+
+  & div {
+    padding-left: 8px;
+  }
+`;
--- src/presentation/pages/Home/Neoga/index.tsx
+++ src/presentation/pages/Home/Neoga/index.tsx
@@ -1,10 +1,22 @@
-import NeogaResultCard from '@components/common/NeogaResultCard';
 import { StBanner, StForm, StHomeNeoga, StResult, StButtonArea } from './styles';
 import { icWhole } from '@assets/icons';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { api } from '@api/index';
+import { NeogaMainCardItem } from '@api/types/neoga';
+import NeogaMainCardList from '@components/NeogaMainCard/List';
+import NeogaResultCard from '@components/common/NeogaResultCard';
 
 function HomeNeoga() {
   const navigate = useNavigate();
+  const [templateList, setTemplateList] = useState<NeogaMainCardItem[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await api.neogaService.getMainTemplate();
+      setTemplateList(data);
+    })();
+  }, []);
 
   return (
     <StHomeNeoga>
@@ -18,6 +30,13 @@ function HomeNeoga() {
             <img src={icWhole} />
           </button>
         </div>
+        <NeogaMainCardList
+          cards={templateList}
+          onItemClick={(id) => {
+            // navigate(`/neoga/${id}`);
+            console.log(id);
+          }}
+        />
       </StForm>
       <StResult>
         <h1>지연님이 만든 너가소개서</h1>

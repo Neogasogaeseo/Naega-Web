7761c5435329abd2fb4d0485678b2828f7573021
Hyoin-Kim <hi980506@naver.com>
2022-01-19 03:30:27 +0900

    🔨refactor: 코드 정리

--- src/infrastructure/api/login-user.ts
+++ src/infrastructure/api/login-user.ts
@@ -5,15 +5,15 @@ export interface LoginUserService {
   getUserInfo(token: string): Promise<LoginUser>;
 }
 
-export const postLogin = async (kakaoToken:string) : Promise<{authUser?:object;accesstoken:string;refreshtoken:string}> => {
+export const postLogin = async (kakaoToken:string) : Promise<{user?:object;accesstoken:string;refreshtoken:string}> => {
   try{
     const response = await publicAPI.post({url:`/auth/login`,data:{authenticationCode:kakaoToken, provider:'kakao'} })
     console.log(response);
     if(response.status===200) return response.data
-    else throw '로그인 실패'
+    else throw '로그인 실패';
   }catch (e){
     console.error(e)
-    throw '로그인 실패' //후에 처리할거
+    throw '로그인 실패';
    }
 }
 
@@ -21,12 +21,11 @@ export const postJoin = async (joinData: FormData) => {
   try {
     const response = await publicAPI.post({url:`/auth/register`,data: joinData, type:'multipart'});
     if(response.status === 200){
-      return response.data
+      return response.data;
     }
-    throw '회원가입 실패'
+    throw '회원가입 실패';
   }catch (e){
     console.error(e)
-    throw '회원가입 실패'
+    throw '회원가입 실패';
   }
-
 }

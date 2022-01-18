8950a4a4f2c5ff49b4879e778b8464a0c356ff59
Hyoin-Kim <hi980506@naver.com>
2022-01-18 19:47:55 +0900

    📝chore: 리다이렉트 uri export 해오기

--- src/presentation/components/LoginForm/OAuth.ts
+++ src/presentation/components/LoginForm/OAuth.ts
@@ -1,4 +1,4 @@
 export const CLIENT_ID = `${process.env.REACT_APP_CLIENT_ID}`;
-const REDIRECT_URI = 'http://localhost:3000/auth/kakao/callback';
+export const REDIRECT_URI = 'http://localhost:3000/auth/kakao/callback';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;

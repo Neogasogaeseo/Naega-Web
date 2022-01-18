74cc85018093bedfa7dc46d33875f1faf717df8e
Hyoin-Kim <hi980506@naver.com>
2022-01-18 19:46:39 +0900

    ✨feat: recoil 사용하여 로그인 토큰 값 관리

--- src/application/stores/kakao-auth.ts
+++ src/application/stores/kakao-auth.ts
@@ -4,4 +4,13 @@ export const kakaoAuthTokenState = atom<string>({
     key: 'kakaoAuthTokenState',
     default: '',
   });
+
+export const kakaoAccessToken = atom<string>({
+  key: 'kakaoAccessToken',
+  default: '',
+});
   
+export const kakaoRefreshToken = atom<string>({
+  key: 'kakaoRefreshToken',
+  default: '',
+});

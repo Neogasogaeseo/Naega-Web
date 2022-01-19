d0d07be2f0796e4fb64f10132eb7818fa716a313
Hyoin-Kim <hi980506@naver.com>
2022-01-20 01:12:57 +0900

    📝chore: 필요한 에셋 추가하기

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -13,3 +13,7 @@ export { default as imgEmptyMain } from './img_empty_main.svg';
 export { ReactComponent as ImgAnswerDone } from './img_answerdone.svg';
 export { default as ImgMainPage } from './img_main_character.svg';
 export { ReactComponent as ImgMainPhone } from './img_landing_phone.svg';
+export { default as ImgGlass } from './img_glass.webp';
+export { default as ImgLink } from './img_link.svg';
+export { default as ImgMypage } from './img_mypage.webp';
+export { default as ImgTeamsoseo } from './img_teamsoseo.webp';

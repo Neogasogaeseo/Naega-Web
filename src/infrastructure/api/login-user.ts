79a6c76584309c5cbb59f6b62bf093ed0eff2a8d
Hyoin-Kim <hi980506@naver.com>
2022-01-19 21:27:51 +0900

    ✨feat: 중복 제거 구현예정

--- src/infrastructure/api/login-user.ts
+++ src/infrastructure/api/login-user.ts
@@ -28,12 +28,19 @@ export const postJoin = async (joinData: FormData) => {
       data: joinData,
       type: 'multipart',
     });
-    if (response.status === 200) {
-      return response.data;
+    try{
+      if (response.status === 200) {
+        return response.data;
+      }
+    }catch(e){
+      if( response.status === 400 ){
+        console.log("중복발생");
+        return response.status;
+      }
     }
     throw '회원가입 실패';
   } catch (e) {
     console.error(e);
-    throw '회원가입 실패';
+    throw '회원가입 실패00';
   }
 };

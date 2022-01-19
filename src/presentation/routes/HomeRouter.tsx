2a240585edb48e6da5f9063f880e243c09f42507
seojin kim <001106ksj@gmail.com>
2022-01-19 23:15:32 +0900

    ✨feat: private router 처리하기

--- src/presentation/routes/HomeRouter.tsx
+++ src/presentation/routes/HomeRouter.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import HomeMyPage from '@pages/Home/MyPage';
 import HomeTeam from '@pages/Home/Team';
 import HomeNeoga from '@pages/Home/Neoga';
+import PrivateRoute from './common/PrivateRoute';
 
 function HomeRouter() {
   return (
     <Routes>
-      <Route path="/" element={<HomeNeoga />} />
-      <Route path="neoga" element={<HomeNeoga />} />
-      <Route path="team" element={<HomeTeam />} />
+      <Route path="/" element={<PrivateRoute />}>
+        <Route path="/" element={<HomeNeoga />} />
+        <Route path="neoga" element={<HomeNeoga />} />
+        <Route path="team" element={<HomeTeam />} />
+      </Route>
       <Route path="mypage/:userID" element={<HomeMyPage />} />
     </Routes>
   );
--- src/presentation/routes/TeamRouter.tsx
+++ src/presentation/routes/TeamRouter.tsx
@@ -6,16 +6,19 @@ import TeamMain from '@pages/Team/Main';
 import TeamRegister from '@pages/Team/Register';
 import TeamRegisterMembers from '@pages/Team/Register/Members';
 import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from './common/PrivateRoute';
 
 const TeamRouter = () => (
   <Routes>
-    <Route path="/register" element={<TeamRegister />} />
-    <Route path="/register/members" element={<TeamRegisterMembers />} />
-    <Route path="/:teamID" element={<TeamMain />} />
-    <Route path="/:teamID/create" element={<TeamNewIssue />} />
-    <Route path="/:teamID/:issueID/*" element={<TeamIssue />}>
-      <Route path="create/*" element={<TeamIssueFeedback />}>
-        <Route path="keyword" element={<TeamIssueKeyword />} />
+    <Route path="/" element={<PrivateRoute />}>
+      <Route path="/register" element={<TeamRegister />} />
+      <Route path="/register/members" element={<TeamRegisterMembers />} />
+      <Route path="/:teamID" element={<TeamMain />} />
+      <Route path="/:teamID/create" element={<TeamNewIssue />} />
+      <Route path="/:teamID/:issueID/*" element={<TeamIssue />}>
+        <Route path="create/*" element={<TeamIssueFeedback />}>
+          <Route path="keyword" element={<TeamIssueKeyword />} />
+        </Route>
       </Route>
     </Route>
   </Routes>

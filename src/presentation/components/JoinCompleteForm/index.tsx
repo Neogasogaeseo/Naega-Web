414fbee40070e3718e9a8de00e08954322266b77
Hyoin-Kim <hi980506@naver.com>
2022-01-20 01:17:29 +0900

    🔨refactor: 코드리뷰 반영

--- src/presentation/components/JoinCompleteForm/index.tsx
+++ src/presentation/components/JoinCompleteForm/index.tsx
@@ -1,19 +1,18 @@
 import { StJoinCompleteForm, StButton, StNoticeWrapper, StTitleWrapper } from './style';
 import { imgParty } from '@assets/images';
 import { useNavigate } from 'react-router-dom';
-import { loginUserState } from '@stores/login-user';
-import { useRecoilState } from 'recoil';
+import { useLoginUser } from '@hooks/useLoginUser';
 
 const JoinCompleteForm = () => {
   const navigate = useNavigate();
-  const [loginUser] = useRecoilState(loginUserState);
+  const { username } = useLoginUser();
 
   return (
     <>
       <StJoinCompleteForm>
         <img src={imgParty} alt="" />
         <StTitleWrapper>
-          <StNoticeWrapper>{loginUser.username}님 어서오세요!</StNoticeWrapper>
+          <StNoticeWrapper>{username}님 어서오세요!</StNoticeWrapper>
           <StNoticeWrapper>회원가입이 완료되었어요</StNoticeWrapper>
         </StTitleWrapper>
         <p>너가소개서를 이용해보세요!</p>
--- src/presentation/pages/OAuthRedirectHandler/index.tsx
+++ src/presentation/pages/OAuthRedirectHandler/index.tsx
@@ -18,7 +18,7 @@ const OAuthRedirectHandler = () => {
         const { id, profileId, name, image } = response.user;
         const accessToken = response.accesstoken;
         saveLoginUser({ id, accessToken, username: name, userID: profileId, profileImage: image });
-        navigate('/join/complete');
+        navigate('/home');
       } else {
         setKakaoAccessToken(response.accesstoken);
         setKakaoRefreshToken(response.refreshtoken ?? '');

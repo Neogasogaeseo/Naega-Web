a3b2054be07d7217db479a0753cbc43a4b4c74a3
seojin kim <001106ksj@gmail.com>
2022-01-20 16:56:32 +0900

    ✨feat: 북마크 API 붙이기

--- src/presentation/components/FeedbackCard/Item/index.tsx
+++ src/presentation/components/FeedbackCard/Item/index.tsx
@@ -3,6 +3,7 @@ import { FeedbackDetail } from '@api/types/team';
 import { icDot } from '@assets/icons';
 import ImmutableKeywordList from '@components/common/Keyword/ImmutableList';
 import { useLoginUser } from '@hooks/useLoginUser';
+import { useToast } from '@hooks/useToast';
 import { useState } from 'react';
 import { StFeedbackCard, StHeader, StBody, StBookmark } from './style';
 
@@ -12,10 +13,15 @@ function FeedbackCardItem(props: FeedbackCardProps) {
   const { id, writer, target, body, createdAt, keywordList, targetProfileID } = props;
   const [isBookmarked, setIsBookmarked] = useState(props.isBookmarked);
   const { id: loginUserID } = useLoginUser();
+  const { fireToast } = useToast();
 
   const bookmarkFeedback = async () => {
     const response = await api.teamService.postFeedbackBookmark(id);
-    if (response.isSuccess) setIsBookmarked((prev) => !prev);
+    if (response.isSuccess) {
+      setIsBookmarked((prev) => response.isBookmarked ?? prev);
+      if (response.isBookmarked)
+        fireToast({ content: 'MY에서 저장된 피드백을 확인할 수 있어요', bottom: 120 });
+    }
   };
 
   return (

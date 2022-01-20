008df31e648cc97f7053c9ea5a85d970fbbb0c7c
Hyoin-Kim <hi980506@naver.com>
2022-01-20 20:24:53 +0900

    ✨feat: 서버 api 연결하기

--- src/infrastructure/remote/issue.ts
+++ src/infrastructure/remote/issue.ts
@@ -1,31 +1,32 @@
 import { privateAPI } from './base';
-import {IssueCategory} from '@api/types/team';
+import { IssueCategory } from '@api/types/team';
 
-export const getTeamIssueCategory = async ():Promise<IssueCategory[] |null> => {
-    const response = await privateAPI.get({url:`/team/issue/category`});
-    if(response.status === 200 ){
-        console.log("response",response.data);
-        return response.data;
-    } else throw '서버 통신 실패';
-}
+export const getTeamIssueCategory = async (): Promise<IssueCategory[] | null> => {
+  const response = await privateAPI.get({ url: `/team/issue/category` });
+  if (response.status === 200) {
+    console.log('response', response.data);
+    return response.data;
+  } else throw '서버 통신 실패';
+};
 
 export const postTeamIssue = async (issueData: FormData) => {
-    try{
-        const response = await privateAPI.post({
-            url:`/team/issue`,
-            data: issueData,
-            type: 'multipart',
-        })
-        .catch(error=> {
-            console.log(error.response);
-        })
-        console.log("response", response.data);
-        if(response.status === 200 ) {
-            return response.data;
-        } else {
-            return response;
-        }
-    }catch(e){
-        throw '데이터 전송 실패';
+  try {
+    const response = await privateAPI
+      .post({
+        url: `/team/issue`,
+        data: issueData,
+        type: 'multipart',
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+    console.log('response', response.data);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      return response;
     }
-}
+  } catch (e) {
+    throw '데이터 전송 실패';
+  }
+};

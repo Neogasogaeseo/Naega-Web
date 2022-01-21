8fa77268faacfce47e8114110c414b6f1059f35e
seojin kim <001106ksj@gmail.com>
2022-01-21 17:08:28 +0900

    ⛑hotfix: 도메인 url에 주석 지우기

--- src/application/utils/constant.ts
+++ src/application/utils/constant.ts
@@ -1,2 +1,2 @@
 export const isProduction = process.env.NODE_ENV === 'production';
-export const DOMAIN = isProduction ? 'https://neogasogaeseo.com/' : 'http://localhost:3000/';
+export const DOMAIN = isProduction ? 'https://neogasogaeseo.com' : 'http://localhost:3000';

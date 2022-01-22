500e8d8a61efe0cdf09fb7f6535a6f3ccbbc960a
seojin kim <001106ksj@gmail.com>
2022-01-22 15:57:01 +0900

    ✨feat: 로그인 된 사용자는 랜딩 페이지를 볼 수 없도록

--- src/presentation/pages/Landing/index.tsx
+++ src/presentation/pages/Landing/index.tsx
@@ -1,6 +1,11 @@
 import LandingPage from '@components/LandingPage';
+import { useLoginUser } from '@hooks/useLoginUser';
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+  const { isAuthenticated } = useLoginUser();
+  const navigate = useNavigate();
+  if (isAuthenticated) navigate('/home');
   return <LandingPage />;
 }
 

7b6eafc1a7bae56899a02b3c3a860402e092b14b
100Gyeon <bjy9966@naver.com>
2022-02-09 17:37:29 +0900

    🐛fix: 토스트 pc에서 이상하게 보이는 문제 해결

--- src/presentation/components/common/Toast/List/style.ts
+++ src/presentation/components/common/Toast/List/style.ts
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 export const StToastList = styled.div`
   bottom: 0;
   left: 0;
+  right: 0;
   position: fixed;
   z-index: 1000;
-  width: calc(100vw - 40px);
+  width: calc(100% - 40px);
+  max-width: 350px;
+  margin: 0 auto;
 `;

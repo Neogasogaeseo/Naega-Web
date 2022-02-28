7d9c8c0dbedbc363e22b2f81355b46968e8bca51
jynam <njy1007@gmail.com>
2022-03-01 01:02:31 +0900

    🔨 refacter: 팀원 등록 페이지 삭제(라우터에서 삭제)

--- src/presentation/routes/TeamRouter.tsx
+++ src/presentation/routes/TeamRouter.tsx
@@ -7,13 +7,11 @@ const TeamIssue = lazy(() => import('@pages/Team/Issue'));
 const TeamNewIssue = lazy(() => import('@pages/Team/Issue/NewIssue'));
 const TeamMain = lazy(() => import('@pages/Team/Main'));
 const TeamRegister = lazy(() => import('@pages/Team/Register'));
-const TeamRegisterMembers = lazy(() => import('@pages/Team/Register/Members'));
 
 const TeamRouter = () => (
   <Routes>
     <Route path="/" element={<PrivateRoute />}>
       <Route path="/register" element={<TeamRegister />} />
-      <Route path="/register/members" element={<TeamRegisterMembers />} />
       <Route path="/:teamID" element={<TeamMain />} />
       <Route path="/:teamID/create" element={<TeamNewIssue />} />
       <Route path="/:teamID/:issueID/*" element={<TeamIssue />}>

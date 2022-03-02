848b247f400d29fda961e3272e9b255b2471c2d9
seojin kim <001106ksj@gmail.com>
2022-03-03 04:46:55 +0900

    ✨ feat: 스크롤 높이로 맨 아래인지 확인하는 훅 만들기

---
+++ src/application/hooks/useScrollHeight.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export function useScrollHeight() {
+  const [isInitialState, setIsInitialState] = useState(true);
+  const [isBottomReached, setIsBottomReached] = useState([]);
+  const handleScroll = useCallback(() => {
+    const scrollHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight,
+    );
+    const scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    const documentHeight = document.documentElement.clientHeight;
+    if (scrollTop + documentHeight >= scrollHeight - 30) {
+      setIsBottomReached([]);
+      setIsInitialState(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll);
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return { isBottomReached, isInitialState };
+}

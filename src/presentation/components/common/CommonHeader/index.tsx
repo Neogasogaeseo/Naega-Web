8f870e360f89674291a084b2577e9cb2de79297a
jynam <njy1007@gmail.com>
2022-03-13 02:00:02 +0900

    ✨ feat: 로고 누르면 홈으로 이동하게 하기

--- src/presentation/components/common/CommonHeader/index.tsx
+++ src/presentation/components/common/CommonHeader/index.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 import { IcBell, IcSetting } from '@assets/icons';
 import { ImgLogoHeader } from '@assets/images';
 import { StCommonHeader } from './style';
 
 export default function CommonHeader() {
+  const navigate = useNavigate();
   return (
     <StCommonHeader>
       <div>
-        <ImgLogoHeader />
+        <ImgLogoHeader
+          onClick={() => {
+            navigate('/home');
+          }}
+        />
         <div>
           <IcSetting />
           <IcBell />

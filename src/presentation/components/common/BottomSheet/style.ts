0b06309e9d034a801b25e66f24ba3c2ecdf1359c
seojin kim <001106ksj@gmail.com>
2022-03-12 21:20:09 +0900

    ✨ feat: Common Bottom Sheet 컴포넌트 작성

---
+++ src/presentation/components/common/BottomSheet/style.ts
@@ -0,0 +1,62 @@
+import { ANIMATION } from '@styles/common/animation';
+import { COLOR } from '@styles/common/color';
+import { FONT_STYLES } from '@styles/common/font-style';
+import styled from 'styled-components';
+
+export const StAbsoluteWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+`;
+
+export const StBlackBlur = styled.div<{ isClosing: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(0, 0, 0, 0.44);
+  z-index: 50;
+
+  animation: ${({ isClosing }) => (isClosing ? ANIMATION.FADE_OUT : ANIMATION.FADE_IN)} 1s;
+`;
+
+export const StWrapper = styled.div<{ isClosing: boolean }>`
+  position: fixed;
+  width: 100%;
+  border-top-left-radius: 32px;
+  border-top-right-radius: 32px;
+  background-color: ${COLOR.WHITE};
+  bottom: 0;
+  z-index: 60;
+  animation: ${({ isClosing }) =>
+      isClosing ? ANIMATION.SWIPE_DOWN : ANIMATION.SWIPE_UP({ from: 0 })}
+    1s;
+`;
+
+export const StButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: ${COLOR.GRAY_7};
+  ${FONT_STYLES.R_16_BODY}
+`;
+
+export const StButtonWrapper = styled.div`
+  padding-top: 30px;
+  padding-bottom: 35px;
+  padding-left: 18px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+export const StCancelButton = styled.div`
+  text-align: center;
+  border-top: 1px solid ${COLOR.GRAY_3};
+  padding-top: 16px;
+  padding-bottom: 52px;
+  color: ${COLOR.GRAY_6};
+  ${FONT_STYLES.M_16_TITLE}
+`;

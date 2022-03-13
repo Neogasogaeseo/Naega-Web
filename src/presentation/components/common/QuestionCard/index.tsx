b1b6278d3fd8f27dae939c0b4f78586138d17a9d
jynam <njy1007@gmail.com>
2022-03-14 03:49:38 +0900

    🎨 design: QuestionCard 뒷면 디자인도 포함할 수 있도록 수정

--- src/presentation/components/common/QuestionCard/index.tsx
+++ src/presentation/components/common/QuestionCard/index.tsx
@@ -1,19 +1,28 @@
-import { StCircle, StQuestionCard } from './style';
+import { imgLinkCreated } from '@assets/images';
+import { StSuggest, StCircle, StQuestionCard, StFormTitle } from './style';
 
 interface QuestionCardProps {
   image: string;
   title: string;
   content: string;
   children: React.ReactNode;
+  isBack?: boolean;
 }
 
 export default function QuestionCard(props: QuestionCardProps) {
-  const { image, title, content, children } = props;
+  const { image, title, content, children, isBack = false } = props;
   return (
-    <StQuestionCard>
-      <img src={image} />
-      <div>{content}</div>
-      <div>{title}</div>
+    <StQuestionCard isBack={isBack}>
+      <img src={isBack ? imgLinkCreated : image} />
+      <div>{isBack ? '너가소개서 생성 완료!' : content}</div>
+      {isBack ? (
+        <StSuggest>
+          <div>링크를 복사해서</div>
+          <div>친구들에게 공유해보세요</div>
+        </StSuggest>
+      ) : (
+        <StFormTitle>{title}</StFormTitle>
+      )}
       <div>
         <StCircle />
         <hr />
--- src/presentation/components/common/QuestionCard/style.ts
+++ src/presentation/components/common/QuestionCard/style.ts
@@ -2,17 +2,17 @@ import { COLOR } from '@styles/common/color';
 import { FONT_STYLES } from '@styles/common/font-style';
 import styled from 'styled-components';
 
-export const StQuestionCard = styled.div`
+export const StQuestionCard = styled.div<{ isBack: boolean }>`
   width: 284px;
   height: 364px;
-  background-color: ${COLOR.WHITE};
+  background-color: ${(props) => (props.isBack ? COLOR.CORAL_MAIN : COLOR.WHITE)};
   position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   border: 1px solid ${COLOR.GRAY_15};
   border-radius: 18px;
-  & > img {
+  & > *:nth-child(1) {
     margin-top: 42px;
   }
   & > *:nth-child(2) {
@@ -23,18 +23,10 @@ export const StQuestionCard = styled.div`
     line-height: 143.99%;
     text-align: center;
     letter-spacing: -0.01em;
-    color: ${COLOR.GRAY_7};
+    color: ${(props) => (props.isBack ? COLOR.WHITE : COLOR.GRAY_7)};
     width: 100%;
     margin-top: 28px;
     white-space: pre-line;
-  }
-  & > *:nth-child(3) {
-    ${FONT_STYLES.R_15_TITLE}
-    color: ${COLOR.GRAY_5};
-    line-height: 100%;
-    text-align: center;
-    letter-spacing: -0.01em;
-    margin-top: 15px;
   }
   & > *:nth-child(4) {
     display: flex;
@@ -48,6 +40,30 @@ export const StQuestionCard = styled.div`
       border: 1px dashed ${COLOR.GRAY_2};
     }
   }
+`;
+
+export const StSuggest = styled.div`
+  margin-top: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ${FONT_STYLES.R_15_TITLE}
+  line-height: 100%;
+  letter-spacing: -0.01em;
+  color: ${COLOR.WHITE};
+  opacity: 0.7;
+  & > *:last-child {
+    margin-top: 8px;
+  }
+`;
+
+export const StFormTitle = styled.div`
+  ${FONT_STYLES.R_15_TITLE}
+  color: ${COLOR.GRAY_5};
+  line-height: 100%;
+  text-align: center;
+  letter-spacing: -0.01em;
+  margin-top: 12px;
 `;
 
 export const StCircle = styled.div`

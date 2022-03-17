b2d327440e6626b20b8e7cad163434096a3757c9
100Gyeon <bjy9966@naver.com>
2022-03-18 05:44:17 +0900

    ✨feat: 로그인된 유저 정보 띄우기

--- src/presentation/pages/Edit/Profile/index.tsx
+++ src/presentation/pages/Edit/Profile/index.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 
+import { useLoginUser } from '@hooks/useLoginUser';
 import CommonInput from '@components/common/CommonInput';
 import CommonLabel from '@components/common/CommonLabel';
 import CommonNavigation from '@components/common/CommonNavigation';
 import FileUpload from '@components/common/FileUpload';
-import { StMyProfileEdit, StProfileImg } from './style';
+import { StInputWrapper, StMyProfileEdit, StProfileImg } from './style';
 import { icEmail, IcMypageEdit } from '@assets/icons';
 import { imgEmptyProfile } from '@assets/images';
 
 function MyProfileEdit() {
   const [image, setImage] = useState<File | null>(null);
+  const { username, userID, profileImage } = useLoginUser();
 
   return (
     <>
@@ -18,7 +20,7 @@ function MyProfileEdit() {
         <StProfileImg>
           <FileUpload width="118px" height="118px" setFile={setImage} borderRadius="50%">
             <>
-              <img src={imgEmptyProfile} />
+              <img src={profileImage || imgEmptyProfile} />
               <IcMypageEdit
                 onClick={() => {
                   console.log(image);
@@ -27,11 +29,13 @@ function MyProfileEdit() {
             </>
           </FileUpload>
         </StProfileImg>
-        <CommonLabel content="아이디" marginTop="52px" marginBottom="20px" />
-        <CommonInput width="100%" placeholder="neososeo_team" maxLength={15} img={icEmail} />
-        <CommonLabel content="이름" marginTop="46px" marginBottom="20px" />
-        <CommonInput width="100%" placeholder="이름을 입력해주세요" />
-        <button>완료</button>
+        <StInputWrapper>
+          <CommonLabel content="아이디" marginTop="52px" marginBottom="20px" />
+          <CommonInput width="100%" placeholder={userID} maxLength={15} img={icEmail} />
+          <CommonLabel content="이름" marginTop="46px" marginBottom="20px" />
+          <CommonInput width="100%" placeholder={username} />
+        </StInputWrapper>
+        <button disabled={true}>완료</button>
       </StMyProfileEdit>
     </>
   );

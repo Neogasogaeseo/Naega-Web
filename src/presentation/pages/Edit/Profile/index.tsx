edd999027705d72ba4042449dfa925e8c5b7baa6
100Gyeon <bjy9966@naver.com>
2022-04-01 02:34:23 +0900

    ✨feat: input 조건 검사 추가

--- src/presentation/pages/Edit/Profile/index.tsx
+++ src/presentation/pages/Edit/Profile/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { useLoginUser } from '@hooks/useLoginUser';
 import CommonInput from '@components/common/Input';
@@ -10,8 +11,47 @@ import { IcMypageEdit } from '@assets/icons';
 import { imgEmptyProfile } from '@assets/images';
 
 function MyProfileEdit() {
-  const [image, setImage] = useState<File | null>(null);
+  const navigate = useNavigate();
   const { username, userID, profileImage } = useLoginUser();
+  const [image, setImage] = useState<File | null>(null);
+  const [inputId, setInputId] = useState('');
+  const [inputName, setInputName] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [isEditConditionPassed, setIsEditConditionPassed] = useState({
+    id: false,
+    name: false,
+  });
+
+  useEffect(() => {
+    const idCheck = /^[a-z|0-9|.|_]{4,15}$/;
+    const idStartCheck = /^[a-z]/;
+
+    setIsEditConditionPassed({
+      id: idCheck.test(inputId) && idStartCheck.test(inputId),
+      name: inputName !== '',
+    });
+
+    if (!idCheck.test(inputId)) {
+      setErrorMsg('*영문 소문자, 숫자, 특수문자(._) 4~15자 이내');
+    }
+
+    if (!idStartCheck.test(inputId)) {
+      setErrorMsg('*아이디의 첫 글자는 영문 소문자');
+    }
+  }, [inputId, inputName]);
+
+  const editProfile = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      const form = new FormData();
+      form.append('profileId', inputId);
+      form.append('name', inputName);
+      image && form.append('image', image);
+    } catch (error) {
+      console.error(error);
+      navigate('/');
+    }
+  };
 
   return (
     <>
@@ -21,21 +61,39 @@ function MyProfileEdit() {
           <FileUpload width="118px" height="118px" setFile={setImage} borderRadius="50%">
             <>
               <img src={profileImage || imgEmptyProfile} />
-              <IcMypageEdit
-                onClick={() => {
-                  console.log(image);
-                }}
-              />
+              <IcMypageEdit />
             </>
           </FileUpload>
         </StProfileImg>
         <StInputWrapper>
           <CommonLabel content="아이디" marginTop="52px" marginBottom="20px" />
-          <CommonInput width="100%" placeholder={userID} maxLength={15} />
+          <CommonInput
+            width="100%"
+            isConditionPassed={isEditConditionPassed.id}
+            onChange={(value) => {
+              setInputId(value);
+            }}
+            errorMsg={errorMsg}
+            placeholder={userID}
+            maxLength={15}
+          />
           <CommonLabel content="이름" marginTop="46px" marginBottom="20px" />
-          <CommonInput width="100%" placeholder={username} />
+          <CommonInput
+            width="100%"
+            isConditionPassed={isEditConditionPassed.name}
+            onChange={(value) => {
+              setInputName(value);
+            }}
+            errorMsg={errorMsg}
+            placeholder={username}
+          />
         </StInputWrapper>
-        <button disabled={true}>완료</button>
+        <button
+          onClick={editProfile}
+          disabled={!Object.values(isEditConditionPassed).every((condition) => condition === true)}
+        >
+          완료
+        </button>
       </StMyProfileEdit>
     </>
   );

13ff83fd8652598947f51754166138fbcb2795d0
100Gyeon <bjy9966@naver.com>
2022-04-01 21:56:01 +0900

    ✨feat: 팀장 여부에 따라 다르게 보이는 바텀시트 만들기

--- src/presentation/pages/Team/Main/index.tsx
+++ src/presentation/pages/Team/Main/index.tsx
@@ -4,25 +4,32 @@ import { useQuery } from 'react-query';
 
 import { api } from '@api/index';
 import { MAX_TEAM_MEMBER } from '@utils/constant';
+import { useLoginUser } from '@hooks/useLoginUser';
 import CommonNavigation from '@components/common/Navigation';
 import IssueCardList from '@components/common/IssueCardList';
+import TeamMainBottomSheet from '@components/common/BottomSheet/TeamMain';
 import TeamMemberPopup from './MemberPopup';
 import { StTeamMain, StTeamInfo, StCheckWrapper, StMemberName, StOtherMember } from './style';
 import { icPerson, icCoralCheck, icGrayCheck, IcMeatball } from '@assets/icons';
 import { imgEmptyProfile } from '@assets/images';
 
 function TeamMain() {
+  const navigate = useNavigate();
+  const { teamID } = useParams();
+  const { username } = useLoginUser();
   const [isMemberPopupOpened, setIsMemberPopupOpened] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
-  const { teamID } = useParams();
-  const navigate = useNavigate();
+  const [isBottomSheetOpened, setIsBottomSheetOpened] = useState(false);
   const checkMyIssue = () => setIsChecked((prev) => !prev);
+
   if (teamID === undefined) navigate('/');
 
   const { data: teamInfoData } = useQuery(['teamDetailData', teamID], () =>
     api.teamService.getTeamInfo(Number(teamID)),
   );
   const slicedMemberList = teamInfoData && teamInfoData.teamMemberList.slice(0, MAX_TEAM_MEMBER);
+  const hostName = slicedMemberList && slicedMemberList[0].profileName;
+  const isUserHost = username === hostName;
 
   const { data: teamIssueList } = useQuery(
     ['teamIssueList', teamID],
@@ -36,13 +43,21 @@ function TeamMain() {
     { enabled: isChecked },
   );
 
+  const openBottomSheet = () => {
+    setIsBottomSheetOpened(true);
+  };
+
+  const closeBottomSheet = () => {
+    setIsBottomSheetOpened(false);
+  };
+
   return (
     <>
       <CommonNavigation />
       <StTeamMain>
         {teamInfoData && (
           <StTeamInfo>
-            <IcMeatball />
+            <IcMeatball onClick={() => openBottomSheet()} />
             <img src={teamInfoData.teamDetail.teamImage || imgEmptyProfile} />
             <div>
               <h1>{teamInfoData.teamDetail.teamName}</h1>
@@ -87,6 +102,14 @@ function TeamMain() {
             navigate(`/team/${teamID}/${issueNumber}`);
           }}
         />
+        {teamID && (
+          <TeamMainBottomSheet
+            isOpened={isBottomSheetOpened}
+            closeBottomSheet={closeBottomSheet}
+            isUserHost={isUserHost}
+            teamID={teamID}
+          />
+        )}
       </StTeamMain>
     </>
   );

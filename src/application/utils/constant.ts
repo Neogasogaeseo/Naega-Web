e8542415260a092ff09498da64343eaf994bf86f
jynam <njy1007@gmail.com>
2022-04-04 19:31:07 +0900

    📝 chore: status code 상수 선언

--- src/application/utils/constant.ts
+++ src/application/utils/constant.ts
@@ -3,3 +3,15 @@ export const DOMAIN = isProduction ? 'https://neogasogaeseo.com' : 'http://local
 export const KEYWORD_PAGE = 15;
 export const SCROLL_BOTTOM_PADDING = 30;
 export const MAX_TEAM_MEMBER = 4;
+export const STATUS_CODE = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503,
+  DB_ERROR: 600,
+};

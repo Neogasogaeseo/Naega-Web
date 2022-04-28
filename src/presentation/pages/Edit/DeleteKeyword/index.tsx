670c9662bee7fdc82e851d741a36a2592894a32d
100Gyeon <bjy9966@naver.com>
2022-04-29 05:55:31 +0900

    feat: 완료 버튼 누르면 마이페이지로 이동

--- src/presentation/pages/Edit/DeleteKeyword/index.tsx
+++ src/presentation/pages/Edit/DeleteKeyword/index.tsx
@@ -1,16 +1,20 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import CommonNavigation from '@components/common/Navigation';
 import { StMyKeywordDelete, StMyKeywordHeader } from './style';
 
 function MyKeywordDelete() {
+  const navigate = useNavigate();
   const { userID } = useParams();
 
   if (!userID) return <></>;
 
   return (
     <>
-      <CommonNavigation title="My 키워드" />
+      <CommonNavigation
+        title="My 키워드"
+        submitButton={{ content: '완료', onClick: () => navigate(`/home/mypage/${userID}`) }}
+      />
       <StMyKeywordDelete>
         <StMyKeywordHeader>
           <span>My 키워드</span>

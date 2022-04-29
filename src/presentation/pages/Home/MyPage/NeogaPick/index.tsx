771f8f50cc7ff90b93baae47d4952fe4125861e4
100Gyeon <bjy9966@naver.com>
2022-04-30 00:54:36 +0900

    🎨design: 너가소개서 픽 하기 헤더

--- src/presentation/pages/Home/MyPage/NeogaPick/index.tsx
+++ src/presentation/pages/Home/MyPage/NeogaPick/index.tsx
@@ -5,7 +5,12 @@ function MyNeogaPick() {
   return (
     <>
       <CommonNavigation title="너가소개서 픽 하기" />
-      <StMyNeogaPick></StMyNeogaPick>
+      <StMyNeogaPick>
+        <header>
+          너가소개서에 지인이 남겨준 답변들 중<br />
+          <span>My 프로필에 걸어두고 싶은 답변</span>을 <span>픽</span>해주세요!
+        </header>
+      </StMyNeogaPick>
     </>
   );
 }

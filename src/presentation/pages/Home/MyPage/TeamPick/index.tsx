cd99db3a423976d495c4a0f2a41250449a9f94b6
100Gyeon <bjy9966@naver.com>
2022-04-30 00:54:47 +0900

    🎨design: 팀원소개서 픽 하기 헤더

--- src/presentation/pages/Home/MyPage/TeamPick/index.tsx
+++ src/presentation/pages/Home/MyPage/TeamPick/index.tsx
@@ -5,7 +5,12 @@ function MyTeamPick() {
   return (
     <>
       <CommonNavigation title="팀원소개서 픽 하기" />
-      <StMyTeamPick></StMyTeamPick>
+      <StMyTeamPick>
+        <header>
+          팀원소개서에 팀원이 남겨준 피드백들 중<br />
+          <span>My 프로필에 걸어두고 싶은 피드백</span>을 <span>픽</span>해주세요!
+        </header>
+      </StMyTeamPick>
     </>
   );
 }

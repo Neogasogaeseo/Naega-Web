1179721e212e47b5589bea3043ee2b8eb8d5dd2d
seojin kim <001106ksj@gmail.com>
2022-04-30 15:17:50 +0900

    ✨ feat: 키워드 폰트 색상 추가

--- src/presentation/components/common/Keyword/Item/style.ts
+++ src/presentation/components/common/Keyword/Item/style.ts
@@ -1,17 +1,20 @@
-import { COLOR } from '@styles/common/color';
+import { FONT_STYLES } from '@styles/common/font-style';
 import styled from 'styled-components';
 
-export const StKeywordItem = styled.div<{ color: string }>`
+export const StKeywordItem = styled.div<{ color: string; fontColor: string }>`
   position: relative;
   & > div {
     background-color: ${({ color }) => color};
-    color: ${(props) => (props.color === COLOR.GRAY_2 ? COLOR.BLACK : COLOR.WHITE)};
+    & > div {
+      color: ${({ fontColor }) => fontColor};
+    }
     font-size: 13px;
     border-radius: 18px;
     padding: 7px 12px;
     display: flex;
     justify-content: space-between;
     gap: 8px;
+    ${FONT_STYLES.R_13_TITLE}
   }
   img {
     cursor: pointer;

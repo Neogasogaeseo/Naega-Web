8f552848f311654582ef0e909ec7019cc878d305
seojin kim <001106ksj@gmail.com>
2022-05-03 03:19:04 +0900

    ✨ feat: 피드백 삭제 후 mutate처리

--- src/presentation/components/common/Modal/DeleteFeedback/index.tsx
+++ src/presentation/components/common/Modal/DeleteFeedback/index.tsx
@@ -1,4 +1,6 @@
 import { api } from '@api/index';
+import { useMutation, useQueryClient } from 'react-query';
+import { useParams } from 'react-router';
 import CommonModal from '..';
 
 type DeleteFeedbackModalProps = {
@@ -10,18 +12,24 @@ type DeleteFeedbackModalProps = {
 
 function DeleteFeedbackModal(props: DeleteFeedbackModalProps) {
   const { closeModal, closeBottomSheet, feedbackID, isOpened } = props;
+  const { teamID, issueID } = useParams();
+  const queryClient = useQueryClient();
   const deleteFeedback = async () => {
     const response = await api.teamService.deleteFeedback(feedbackID);
-    if (response.isSuccess) {
+    return response.isSuccess;
+  };
+  const { mutate: mutateDeleteFeedback } = useMutation(deleteFeedback, {
+    onSuccess: () => {
       closeModal();
       closeBottomSheet();
-    }
-  };
+      return queryClient.invalidateQueries(['issueDetailData', `${teamID}-${issueID}`]);
+    },
+  });
   return (
     <CommonModal
       title="피드백을 삭제하시겠습니까?"
       onClickCancel={closeModal}
-      onClickConfirm={deleteFeedback}
+      onClickConfirm={mutateDeleteFeedback}
       isOpened={isOpened}
     />
   );

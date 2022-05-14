3569056b2908b6030203022ee06f9298d5ac9f4a
seojin kim <001106ksj@gmail.com>
2022-05-15 04:24:45 +0900

    ♻️ refactor: lottie를 불러올 때 사용하는 id, querySelector 삭제

--- src/assets/lottie/TeamLottie/index.tsx
+++ src/assets/lottie/TeamLottie/index.tsx
@@ -1,20 +1,22 @@
 import lottie from 'lottie-web';
 import NeogaLottie from './TeamLandingLottie.json';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 function TeamLandingLottie() {
+  const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    const component = document.querySelector('#teamContainer');
-    component &&
+    const container = containerRef.current;
+    if (container)
       lottie.loadAnimation({
-        container: component,
+        container,
         renderer: 'svg',
         loop: true,
         autoplay: true,
         animationData: NeogaLottie,
       });
   }, []);
-  return <div id="teamContainer" style={{ width: '100%', height: '86px' }} />;
+
+  return <div ref={containerRef} style={{ width: '100%', height: '86px' }} />;
 }
 
 export default TeamLandingLottie;

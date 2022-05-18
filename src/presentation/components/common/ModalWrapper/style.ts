fff9dcf63b848997f6daba77522749e00a56b84c
jynam <njy1007@gmail.com>
2022-05-18 17:11:25 +0900

    🐛 fix: 모달 래퍼 스크롤 막는 기능 추가

--- src/presentation/components/common/ModalWrapper/style.ts
+++ src/presentation/components/common/ModalWrapper/style.ts
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export const StModalWrapper = styled.div<{ isOpened: boolean; isAnimation: boolean }>`
   width: 100%;
+  overflow-y: scroll;
   min-height: 100vh;
-  position: absolute;
+  position: fixed;
   visibility: ${(props) => (props.isOpened ? 'visible' : 'hidden')};
   z-index: 999;
   background-color: rgba(0, 0, 0, 0.6);

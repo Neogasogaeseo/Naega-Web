a72917a1a4365c3ca904483aa17a22d659290473
seojin kim <001106ksj@gmail.com>
2022-05-25 01:27:21 +0900

    🐛 fix: 마이페이지 너가소개서 픽 하기에서 중복 폼이 등장하는 오류 임시 해결

--- src/infrastructure/remote/user.ts
+++ src/infrastructure/remote/user.ts
@@ -4,6 +4,7 @@ import { NotFoundError } from '@api/types/errors';
 import { UserService } from '@api/user';
 import { KEYWORD_PAGE, PICK_PAGE, STATUS_CODE } from '@utils/constant';
 import { imgEmptyProfile } from '@assets/images';
+import { MyDetail } from '@api/types/user';
 
 export function userDataRemote(): UserService {
   const getKeywords = async (userID: number, page: number) => {
@@ -157,10 +158,16 @@ export function userDataRemote(): UserService {
     const response = await privateAPI.get({ url: `/form/answer/pick/form` });
     return {
       formList: response.data.form
-        ? response.data.form.map((form: any) => ({
-            id: form.formId,
-            profileImage: form.darkIconImage,
-          }))
+        ? response.data.form
+            .map((form: any) => ({
+              id: form.formId,
+              profileImage: form.darkIconImage,
+            }))
+            .reduce((acc: MyDetail[], cur: MyDetail) => {
+              if (acc.map((item) => item.id).includes(cur.id)) return acc;
+              acc.push(cur);
+              return acc;
+            }, [])
         : [],
     };
   };

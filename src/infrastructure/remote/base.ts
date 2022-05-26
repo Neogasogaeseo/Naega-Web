ee1e8170ef482e4d61cbc04a0dea95fa4c04625d
100Gyeon <bjy9966@naver.com>
2022-05-26 16:43:29 +0900

    📝chore: ngrok 관련 세팅

--- src/infrastructure/remote/base.ts
+++ src/infrastructure/remote/base.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASEURL = 'https://fbd7-203-229-147-89.ngrok.io/neogasogaeseo-9aaf5/asia-northeast3/api';
+const BASEURL = 'https://b2bc-124-54-85-139.jp.ngrok.io/neogasogaeseo-9aaf5/asia-northeast3/api';
 const getAccessToken = () => localStorage.getItem('token') ?? '';
 
 const getBasePrivateHeaders = () => ({

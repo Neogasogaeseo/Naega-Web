a925d05ed4a14be00572194b985959406ca61b71
100Gyeon <bjy9966@naver.com>
2022-05-26 21:52:19 +0900

    📝chore: base url 변경

--- src/infrastructure/remote/base.ts
+++ src/infrastructure/remote/base.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASEURL = 'https://4df9-1-209-175-101.jp.ngrok.io/neogasogaeseo-9aaf5/asia-northeast3/api';
+const BASEURL = 'https://7612-124-54-85-139.jp.ngrok.io/neogasogaeseo-9aaf5/asia-northeast3/api';
 const getAccessToken = () => localStorage.getItem('token') ?? '';
 
 const getBasePrivateHeaders = () => ({

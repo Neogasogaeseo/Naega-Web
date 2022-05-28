d98be2e64cc711af2b630151d3de6eb00f22d3c3
100Gyeon <bjy9966@naver.com>
2022-05-28 21:06:40 +0900

    📝chore: 불필요한 엠티뷰 이미지 제거

--- src/presentation/components/common/Empty/Feedback/index.tsx
+++ src/presentation/components/common/Empty/Feedback/index.tsx
@@ -1,4 +1,3 @@
-import { imgEmptyFeedback } from '@assets/images';
 import { StFeedbackEmptyView } from './style';
 
 interface FeedbackEmptyViewProps {
@@ -10,7 +9,6 @@ function FeedbackEmptyView(props: FeedbackEmptyViewProps) {
 
   return (
     <StFeedbackEmptyView hasThumbnail={hasThumbnail}>
-      <img src={imgEmptyFeedback} />
       <div>아직 피드백이 없어요</div>
       <div>첫번째 피드백을 남겨주세요</div>
     </StFeedbackEmptyView>

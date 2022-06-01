fba6d0c0deda4733bfea2ab4a3d91c19a74da2c1
jynam <njy1007@gmail.com>
2022-06-01 21:12:05 +0900

    🎨 design: ErrorFallback 퍼블리싱

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -54,3 +54,4 @@ export { default as icLeaveTeam } from './ic_leave_team.svg';
 export { ReactComponent as IcLock } from './ic_lock.svg';
 export { default as icPicked } from './ic_picked.svg';
 export { default as icUnpicked } from './ic_unpicked.svg';
+export { ReactComponent as IcPaperAirplane } from './ic_paper_airplane.svg';

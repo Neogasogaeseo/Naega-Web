796e1f828e2320110c4852338823b5becfa7d66d
jynam <njy1007@gmail.com>
2022-08-30 22:16:32 +0900

    📝 chore: useGetFormInfo 훅 options 커스텀 가능하도록 수정

--- src/application/hooks/queries/neososeo-form.ts
+++ src/application/hooks/queries/neososeo-form.ts
@@ -1,6 +1,17 @@
-import { useQuery } from 'react-query';
+import { AxiosError } from 'axios';
+import { useQuery, UseQueryOptions } from 'react-query';
 
 import { api } from '@api/index';
+import { NeososeoFormData } from '@api/types/neososeo-form';
 
-export const useGetFormInfo = (q: string) =>
-  useQuery(['neososeoForm', q], () => api.neososeoFormService.getFormInfo(q));
+export const useGetFormInfo = (
+  q: string,
+  options?: Omit<
+    UseQueryOptions<NeososeoFormData, AxiosError, Promise<NeososeoFormData>, string[]>,
+    'queryKey' | 'queryFn'
+  >,
+) =>
+  useQuery(['neososeoForm', q], () => api.neososeoFormService.getFormInfo(q), {
+    ...options,
+    useErrorBoundary: options?.useErrorBoundary || true,
+  });

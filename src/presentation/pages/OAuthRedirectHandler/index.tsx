626fc1018faad9a512a032cd9001fa01f0ad456e
jynam <njy1007@gmail.com>
2022-09-15 23:54:07 +0900

    🔨 refactor: 필요 없는 useState, useEffect 삭제

--- src/presentation/pages/OAuthRedirectHandler/index.tsx
+++ src/presentation/pages/OAuthRedirectHandler/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from 'react-query';
 
@@ -8,25 +8,23 @@ import { api } from '@api/index';
 const OAuthRedirectHandler = () => {
   const navigate = useNavigate();
   const { saveLoginUser } = useLoginUser();
-  const [authorizationCode, setAuthorizationCode] = useState<string>('');
 
-  const { mutate: login } = useMutation(() => api.loginUserService.postLogin(authorizationCode), {
-    useErrorBoundary: true,
-    onSuccess: (data) => {
-      saveLoginUser(data);
-      if (data.isJoined) navigate('/home');
-      else navigate('/join');
+  const { mutate: login } = useMutation(
+    (authorizationCode: string) => api.loginUserService.postLogin(authorizationCode),
+    {
+      useErrorBoundary: true,
+      onSuccess: (data) => {
+        saveLoginUser(data);
+        if (data.isJoined) navigate('/home');
+        else navigate('/join');
+      },
     },
-  });
-
-  useEffect(
-    () => setAuthorizationCode(new URL(window.location.href).searchParams.get('code') ?? ''),
-    [],
   );
 
   useEffect(() => {
-    if (authorizationCode.length) login();
-  }, [authorizationCode]);
+    const authorizationCode = new URL(window.location.href).searchParams.get('code') ?? '';
+    if (authorizationCode.length) login(authorizationCode);
+  }, []);
 
   return <></>;
 };

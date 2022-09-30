569081b61f207d0038e5c1f69d25f903ccc2bc86
jynam <njy1007@gmail.com>
2022-09-30 21:42:14 +0900

    📝 chore: 배포 후 테스트 위한 console.log

--- src/application/hooks/useLoginUser.ts
+++ src/application/hooks/useLoginUser.ts
@@ -29,17 +29,25 @@ export function useLoginUser() {
   };
 
   const saveLoginUser = (loginUser: LoginUser) => {
+    console.log('saveLoginUser 1', loginUser);
     setLoginUser(loginUser);
+    console.log('saveLoginUser 2', loginUser);
     setIsAuthenticated(true);
+    console.log('saveLoginUser 3', loginUser);
     localStorage.setItem(TOKEN_KEYS.ACCESS, loginUser.accessToken);
+    console.log('saveLoginUser 4', loginUser);
     localStorage.setItem(TOKEN_KEYS.REFRESH, loginUser.refreshToken);
+    console.log('saveLoginUser 5', loginUser);
   };
 
   const initLoginUser = async () => {
+    console.log('initLoginUser 1');
     try {
       const accessToken = localStorage.getItem(TOKEN_KEYS.ACCESS);
       const refreshToken = localStorage.getItem(TOKEN_KEYS.REFRESH);
+      console.log('initLoginUser 2', accessToken, refreshToken);
       if (accessToken && refreshToken) {
+        console.log('initLoginUser 3', accessToken, refreshToken);
         const user = await api.loginUserService.getUserInfo(accessToken);
         if (user.userID && user.username && user.profileImage) {
           saveLoginUser({
@@ -52,6 +60,7 @@ export function useLoginUser() {
       } else throw new UnauthorizedError('토큰이 없습니다');
     } catch (error) {
       setError(error);
+      console.log('>>>>>알 수 없는 에러', error);
     }
   };
 

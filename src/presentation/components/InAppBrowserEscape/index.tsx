5f945a2a4eb6dca633eb413b73d26130e3ab73b4
Seojin Kim <seojin.kim@voyagerx.com>
2022-10-02 22:11:54 +0900

    ♻️ refactor: InAppBrowserEscape의 변수명 변경

--- src/presentation/components/InAppBrowserEscape/index.tsx
+++ src/presentation/components/InAppBrowserEscape/index.tsx
@@ -2,7 +2,7 @@ import CommonModal from '@components/common/Modal';
 import React, { useCallback, useEffect, useState } from 'react';
 
 const InAppBrowserEscape = () => {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [isModalOpened, setIsModalOpened] = useState(false);
   const openRealBrowser = useCallback(() => {
     const isIOS = navigator.userAgent.match(/iPhone|iPad/i);
     if (isIOS) {
@@ -18,7 +18,7 @@ const InAppBrowserEscape = () => {
     const isInAppBrowser = navigator.userAgent.match(
       /inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\/[^1]/i,
     );
-    if (isInAppBrowser !== null) setModalOpen(true);
+    if (isInAppBrowser !== null) setIsModalOpened(true);
   }, []);
 
   return (
@@ -27,9 +27,9 @@ const InAppBrowserEscape = () => {
       description={
         '더 쾌적하게 너가소개서를 이용하기 위해' + '\n' + '외부 브라우저에서 접속해 주세요.'
       }
-      onClickCancel={() => setModalOpen(false)}
+      onClickCancel={() => setIsModalOpened(false)}
       onClickConfirm={openRealBrowser}
-      isOpened={modalOpen}
+      isOpened={isModalOpened}
     />
   );
 };

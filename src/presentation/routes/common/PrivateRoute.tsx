5d6bf283ddf548d9e167c726c9327b2d7d32ef72
jynam <njy1007@gmail.com>
2022-10-02 09:52:44 +0900

    📝 chore: 배포 후 테스트 위한 코드

--- src/presentation/routes/common/PrivateRoute.tsx
+++ src/presentation/routes/common/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import ErrorGuard from './ErrorGuard';
@@ -8,12 +8,17 @@ import { loginUserState } from '@stores/login-user';
 function PrivateRoute() {
   const { isAuthenticated, isLoading, error } = useLoginUser();
   const loginUser = useRecoilValue(loginUserState);
+  const navigate = useNavigate();
+  console.log('isAuth', isAuthenticated);
 
   if (error) throw error;
 
   if (isLoading) return <></>;
 
-  if (isAuthenticated) return loginUser.isJoined ? <Outlet /> : <Navigate to="/join" />;
+  if (isAuthenticated) {
+    if (loginUser.isJoined) return <Outlet />;
+    else navigate('/join');
+  }
 
   return <Navigate to="/" />;
 }

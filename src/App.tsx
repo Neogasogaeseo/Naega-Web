1cf12bc5f90b1630c9ed277661397a4e48a6f5f1
jynam <njy1007@gmail.com>
2022-10-16 21:07:32 +0900

    📝 chore: vconsole 설정

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ import GlobalStyle from '@styles/global';
 import Router from '@routes/Router';
 import { useLoginUser } from '@hooks/useLoginUser';
 import ToastList from '@components/common/Toast/List';
+import VConsole from 'vconsole';
+import { DOMAIN } from '@utils/constant';
 
 function App() {
+  if (DOMAIN === 'https://naegasogaeseo-dev.kro.kr') new VConsole();
+
   const { initLoginUser } = useLoginUser();
 
   const queryClient = new QueryClient({

490cc3d48c87e8647512a07395ba125f742d7813
jynam <njy1007@gmail.com>
2022-11-26 00:39:17 +0900

    🚨 hotfix: redirect uri 변경

--- src/application/utils/constant.ts
+++ src/application/utils/constant.ts
@@ -1,5 +1,5 @@
 export const isProduction = process.env.NODE_ENV === 'production';
-export const DOMAIN = isProduction ? 'https://naegasogaeseo-dev.kro.kr' : 'http://localhost:3000';
+export const DOMAIN = isProduction ? 'https://neogasogaeseo.com' : 'http://localhost:3000';
 
 export const PAGES = { KEYWORD: 15, NOTICE: 15, PICK: 10, SEARCHED_USER: 16 };
 

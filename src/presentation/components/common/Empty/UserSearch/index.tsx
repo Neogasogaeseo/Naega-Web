627cf761eebc966574e41d386f8363321965b392
100Gyeon <bjy9966@naver.com>
2023-01-16 03:44:58 +0900

    📝chore: 이름 -> 아이디 텍스트 수정

--- src/presentation/components/common/Empty/UserSearch/index.tsx
+++ src/presentation/components/common/Empty/UserSearch/index.tsx
@@ -4,7 +4,7 @@ export default function UserSearchEmptyView() {
   return (
     <StUserSearchEmptyView>
       <div>검색결과 없음</div>
-      <div>팀원 이름을 검색해 팀으로 초대하세요</div>
+      <div>팀원 아이디를 검색해 팀으로 초대하세요</div>
     </StUserSearchEmptyView>
   );
 }
